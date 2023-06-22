5ec4f5247c97e8702c6e3e08700a5c4ed2c51f01
Clement Paluszek <cpalusze@z4r11p1.42lyon.fr>
2023-06-22 14:28:56 +0200

    prisma setup in nest project

--- Docker/Backend/tools/start_nest.sh
+++ Docker/Backend/tools/start_nest.sh
@@ -5,4 +5,8 @@ nest new nest-app --package-manager npm
 sed -i 's/3000/4000/g' ./nest-app/src/main.ts
 cd nest-app
 
+# Setup Prisma ORM
+npm install prisma --save-dev
+npx prisma init
+
 npm run start:dev
