03dcf85609fd076c1ad993baaf648cdb6769ae07
bl000m <mathiapagani@gmail.com>
2023-06-24 21:34:04 +0000

    implemented getallusers method in user.service and randomusergenerator for testing

--- App/Nest/nest_app/src/users/users.service.ts
+++ App/Nest/nest_app/src/users/users.service.ts
@@ -2,12 +2,15 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service'; 
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from '@prisma/client' 
+import { plainToClass } from 'class-transformer';
 
 @Injectable()
 export class UsersService {
     constructor(
         private readonly prisma: PrismaService,
     ) {}
+
+    /* C(reate) */
 
     async createUser(createUserDto: CreateUserDto): Promise<User> {
         const user = await this.prisma.user.create({
@@ -20,4 +23,34 @@ export class UsersService {
         });
         return user;
     } 
+
+    /* R(ead) */
+
+    async getAllUser(): Promise<CreateUserDto[]> {
+        const users = await this.prisma.user.findMany({
+            orderBy: { id: 'asc' },
+        });
+        const userlistDto = users.map(user => plainToClass(CreateUserDto, user)); // transforming each user object into a DTO object
+        return userlistDto;
+    }
+
+    /* testing */
+    async generateRandomUsers(): Promise<void> {
+        const usersToCreate: CreateUserDto[] = [];
+      
+        for (let i = 0; i < 10; i++) {
+          const randomUser: CreateUserDto = {
+            id: i + 1,
+            username: `User${i + 1}`,
+            email: `user${i + 1}@example.com`,
+            avatar: `avatar${i + 1}.jpg`,
+            password: 'password123',
+          };
+      
+          usersToCreate.push(randomUser);
+        }
+      
+        await Promise.all(usersToCreate.map(user => this.prisma.user.create({ data: user })));
+      }
+      
 }
