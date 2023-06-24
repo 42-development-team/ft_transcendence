7453c0ee2e1bbcc74b59d6ae32137bf5618fb2cd
bl000m <mathiapagani@gmail.com>
2023-06-24 16:37:01 +0000

    user data transfer objects v1 DONE

--- App/Nest/nest_app/src/users/dto/create-user.dto.ts
+++ App/Nest/nest_app/src/users/dto/create-user.dto.ts
@@ -1,6 +1,27 @@
+import { Exclude } from 'class-transformer';
+/* By using @Exclude(), sensitive data is not included when converting the UserDto instance 
+to plain objects or JSON.
+This helps protect sensitive information from unintentional exposure or transmission. */
+import { IsNotEmpty, IsString, IsNumber, MaxLength } from 'class-validator';
+ 
  export class CreateUserDto {
+   @IsNotEmpty()
+   @IsNumber()
    id:         number;
+
+   @IsNotEmpty()
+   @IsString()
    username:   string;
+
+   @IsNotEmpty()
+   @IsString()
    email:      string;
+
+   @IsNotEmpty()
+   @IsString()
+   @MaxLength(65_000) //This can help enforce constraints and ensure that the uploaded file path or URL doesn't inadvertently exceed a reasonable length.
+   avatar:     string;
+
+   @Exclude()
    password:   string;
  }
--- App/Nest/nest_app/src/users/users.service.ts
+++ App/Nest/nest_app/src/users/users.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
-// import { PrismaService }
+import { PrismaService } from 'src/prisma/prisma.service'; 
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
 export class UsersService {
