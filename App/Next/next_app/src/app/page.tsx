e6629c3c14282bee2a6d2393be0a903ff43be033
bl000m <mathiapagani@gmail.com>
2023-06-25 22:11:10 +0000

    front home signup section ok

--- App/Next/next_app/src/app/page.tsx
+++ App/Next/next_app/src/app/page.tsx
@@ -1,9 +1,26 @@
 import './globals.css';
+import Head from 'next/head';
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <h2>Hello World!</h2>
+    <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-gray-100">
+      <Head>
+        <title>Transcendence</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
+        <div className='bg-white rounded-2xl shadow-2xl flex w-2/3 max-w-4xl'>
+          <div className='w-3/5 p-5'>
+            <p>Sign in</p>
+          </div>
+          <div className='w-2/5 bg-green-500 text-white rounded-tr-2xl rounded-br-2xl py-36 px-12'>
+            <h2 className='text-3xl font-bold mb-2'>Do you want to play PONG ?</h2>
+            <div className='border-2 w-10 border-white inline-block mb-2'></div>
+            <p className='mb-8'>Insert your 42 login and choose a f*****g password</p>
+            <a href='' className='border-2 border-white rounded-full px-12 py-2 inline-block font-semibold hover:bg-white hover:text-red-500'>Sign up</a>
+          </div>
+        </div>
+      </main>
     </div>
   )
 }
