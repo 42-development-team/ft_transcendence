83ef846a7cd13b9215ab40fc25e45dd27ea988ae
Clement Paluszek <cpalusze@z3r11p3.42lyon.fr>
2023-06-27 15:01:33 +0200

    Add nestjs/swagger

--- App/Nest/nest_app/src/main.ts
+++ App/Nest/nest_app/src/main.ts
@@ -3,9 +3,21 @@ import { AppModule } from './app.module';
 import { AppService } from './app.service';
 import { UsersService } from './users/users.service';
 
+// Swagger
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { doc } from 'prettier';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const appService = app.get(AppService);
+
+  const config = new DocumentBuilder()
+    .setTitle("Transcendence API")
+    .setVersion('0.1')
+    .build()
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(4000);
 
 }
