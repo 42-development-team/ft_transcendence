688d9621a68c31920cb9367dc87f097713c58513
aucaland <aurelien.calandrino@outlook.fr>
2023-07-03 11:32:12 +0200

    Conflict school-home

--- App/Nest/nest_app/src/main.ts
+++ App/Nest/nest_app/src/main.ts
@@ -2,8 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { AppService } from './app/app.service';
 import { UsersService } from './users/users.service';
-
-
 /////////// TEST CORS ///////////////
 
 //////////////////////////////////////
@@ -28,8 +26,17 @@ async function bootstrap() {
     .build()
   const document = SwaggerModule.createDocument(app, config);
   app.useGlobalPipes( new ValidationPipe() );
+  var cors = require('cors')
+
+  app.options('/users', cors()) // enable pre-flight request for DELETE request
+  app.del('/users', cors(), function (req, res, next) {
+    res.json({msg: 'This is CORS-enabled for all origins!'})
+  })
   SwaggerModule.setup('api', app, document);
-  await app.listen(4000);
+  await app.listen(4000, function () {
+    console.log('CORS-enabled web server listening on port 4000')
+  })
+  // await app.listen(4000);
 
 }
 bootstrap();
