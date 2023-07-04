ac2618059343715108bce08d85c8e7ce0d0b432c
aucaland <aurelien.calandrino@outlook.fr>
2023-07-04 15:04:37 +0200

    delete core test
    -nothing works

--- App/Nest/nest_app/src/main.ts
+++ App/Nest/nest_app/src/main.ts
@@ -2,9 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { AppService } from './app/app.service';
 import { UsersService } from './users/users.service';
-/////////// TEST CORS ///////////////
-
-//////////////////////////////////////
 
 // Swagger
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
@@ -26,17 +23,8 @@ async function bootstrap() {
     .build()
   const document = SwaggerModule.createDocument(app, config);
   app.useGlobalPipes( new ValidationPipe() );
-  var cors = require('cors')
-
-  app.options('/users', cors()) // enable pre-flight request for DELETE request
-  app.del('/users', cors(), function (req, res, next) {
-    res.json({msg: 'This is CORS-enabled for all origins!'})
-  })
   SwaggerModule.setup('api', app, document);
-  await app.listen(4000, function () {
-    console.log('CORS-enabled web server listening on port 4000')
-  })
-  // await app.listen(4000);
+  await app.listen(4000);
 
 }
 bootstrap();
