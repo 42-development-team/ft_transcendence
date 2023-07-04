8942cfd7ef67207a6d4735b2f48159c2a1c787a1
aucaland <aurelien.calandrino@outlook.fr>
2023-07-04 15:00:05 +0200

    complete component 2FA
    -Complete component , ready to test 2FA  test

--- App/Next/next_app/src/2fa_components/2fa_status.tsx
+++ App/Next/next_app/src/2fa_components/2fa_status.tsx
@@ -1,11 +1,18 @@
 "use client";
 import React, {useState, useEffect} from "react";
 
+async function toFetch() {
+    const canva = await fetch("http://localhost:4000/2fa/turn-on/:username");
+    const dataFormat = await canva.json();
+    console.log(dataFormat);
+    return (dataFormat);
+}
+
 function enable2FA() {
 	console.log("Enable 2FA");
 }
 
-const enable2FAComponent = () => {
+const Enable2FAComponent = () => {
 	return (
 		<div>
 			<button onClick={enable2FA}>Enable 2FA</button>
@@ -13,4 +20,4 @@ const enable2FAComponent = () => {
 	)
 }
 
-export default enable2FAComponent;
+export default Enable2FAComponent;
