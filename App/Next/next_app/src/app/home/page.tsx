fb7e36d00e4cb724f5289d8177bbd66e351548e4
Clément Paluszek <105011036+Cpaluszek@users.noreply.github.com>
2023-07-06 10:54:04 +0200

    create home page

---
+++ App/Next/next_app/src/app/home/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+        <div className="flex flex-col flex-auto items-center justify-center py-2">
+            <a className="align-baseline"> You should land here after successful login </a>
+        </div>
+    )
+ }
