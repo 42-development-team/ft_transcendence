b4532f79aff8e8fb2b02195c3597404a7575b2df
aucaland <aurelien.calandrino@outlook.fr>
2023-07-07 17:26:27 +0200

    -delete password conflict in prisma schema

--- App/Nest/nest_app/src/users/dto/create-user.dto.ts
+++ App/Nest/nest_app/src/users/dto/create-user.dto.ts
@@ -3,7 +3,6 @@ import { Exclude } from 'class-transformer';
 to plain objects or JSON.
 This helps protect sensitive information from unintentional exposure or transmission. */
 import { IsNotEmpty, IsString, IsNumber, MaxLength, IsBoolean } from 'class-validator';
-import {isStrongPassword} from "../../../custom_decorators/isStrongPassword.decorators";
  
  export class CreateUserDto {
    @IsNotEmpty()
@@ -22,10 +21,6 @@ import {isStrongPassword} from "../../../custom_decorators/isStrongPassword.deco
    @IsString()
    @MaxLength(65_000) //This can help enforce constraints and ensure that the uploaded file path or URL doesn't inadvertently exceed a reasonable length.
    avatar:     string;
-
-   // @Exclude()
-   @isStrongPassword()
-   password:   string;
 
    @IsString()
    twoFAsecret: string;
