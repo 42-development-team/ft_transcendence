5ad7240902f542088d4b7df889dee6baed3b6646
bl000m <mathiapagani@gmail.com>
2023-07-08 14:23:36 +0000

    added websocket packages in package.json

--- App/Nest/nest_app/src/app/app.module.ts
+++ App/Nest/nest_app/src/app/app.module.ts
@@ -4,26 +4,14 @@ import { AppService } from './app.service';
 import { UsersController } from '../users/users.controller';
 import { UsersService } from '../users/users.service';
 import { PrismaService } from '../prisma/prisma.service';
-import { PrismaModule } from '../prisma/prisma.module';
+// import { PrismaModule } from '../prisma/prisma.module';
 import { ChatroomController } from 'src/chatroom/chatroom.controller';
 import { ChatroomService } from 'src/chatroom/chatroom.service';
-<<<<<<< HEAD
-<<<<<<< HEAD
 import { SocketModule } from '@nestjs/platform-socket.io';
 
 
 @Module({
   imports: [SocketModule],
-=======
-
-@Module({
-  imports: [],
->>>>>>> FT-86-implement-backend-logic-to-creating-chat-rooms-managing-participants-and-joining-rooms
-=======
-
-@Module({
-  imports: [],
->>>>>>> FT-86-implement-backend-logic-to-creating-chat-rooms-managing-participants-and-joining-rooms
   controllers: [AppController, UsersController, ChatroomController],
   providers: [AppService, UsersService, ChatroomService, PrismaService],
 })
