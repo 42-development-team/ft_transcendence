bff059c7f41f2631bd0acfb6268bba8c9a8b4688
aucaland <aurelien.calandrino@outlook.fr>
2023-07-10 13:46:37 +0200

    create foders
    -component/auth/2fa_auth
    -rename 2fa_management in 2fa_settings_management
    -create src/auth/2fa/page.tsx

--- App/Next/next_app/src/app/auth/page.tsx
+++ App/Next/next_app/src/app/auth/page.tsx
@@ -1,10 +1,10 @@
 "use client";
 import Manage2FAComponent from "../components/auth/2fa_settings_management";
-export default function Settings() {
+export default function TwoFAAuth() {
     // const {isLoggedIn, login, logout} = useLoggedInContext();
     return (
         <div className="flex flex-col flex-auto items-center justify-center">
-            <Manage2FAComponent></Manage2FAComponent>
+            <TwoFAAuthComponent></TwoFAAuthComponent>
         </div>
     )
   }
---
+++ App/Next/next_app/src/app/components/auth/2fa_auth_mangement.tsx
@@ -0,0 +1,131 @@
+"use client";
+import React, {useState, useEffect} from "react";
+import CustomBtn from "../CustomBtn";
+import '../../globals.css'
+
+const TwoFAAuthComponent = () => {
+	const [imageUrl, setImageUrl] = useState<string>('');
+	const [isActive, setIsActive] = useState<boolean>(false);
+	const [inputValue, setInputValue] = useState('');
+	const [displayBox, setDisplayBox] = useState<Boolean>(false);
+
+	useEffect( () => {
+		isTwoFAActive();
+	}, [] );
+
+	const isTwoFAActive = async () => {
+		const response = await fetch('http://localhost:4000/2fa/isTwoFAActive/dfbsurain');
+		if (!response.ok) {
+			throw new Error('Failed to fetch \'isTwoFAActive');
+		}
+		const data = await response.json();
+		console.error("isActive?:" + data);
+		setIsActive(data);
+	}
+
+	const isTwoFAValid = async () => {
+
+		const response = await fetch('http://localhost:4000/2fa/verifyTwoFA/dfbsurain', {
+			method: 'POST',
+			body: JSON.stringify({code: inputValue}),
+			headers: {
+		'Content-Type': 'application/json',
+		}});
+		const data = await response.json();
+		console.log("isValid?: " + data);
+		return data;
+	}
+
+	const handleEnableClick = async () => {
+		try {
+			setDisplayBox(true);
+			const response = await fetch('http://localhost:4000/2fa/turn-on/dfbsurain');
+			const data = await response.json();
+			setImageUrl(data.base64Qrcode);
+		} catch (error)                                                                                                                                                                                                                                                                                                                                                                                                                 {
+			console.error('Error retrieving image URL:', error);
+		}
+	};
+
+	const handleDisableClick = async () => {
+		try {
+			setDisplayBox(true);
+			setImageUrl('');
+
+		}
+		catch(error) {
+			console.error('Error in handleDisableClick', error);
+		}
+	};
+
+	const turnOff = async () => {
+		const response = await fetch('http://localhost:4000/2fa/turn-off/dfbsurain');
+		const data = await response.json();
+		if (data.ok)
+			return ;
+	}
+
+	const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setInputValue(event.target.value);
+	}
+
+	const handleSubmit = async () => {
+		const isValid = await isTwoFAValid();
+		if (!isValid)
+			{
+				setIsVisible(true);
+				setMessage("Wrong code");
+				return ;
+			}
+		if (isActive) {
+			turnOff();
+			setIsActive(false);
+			setImageUrl('');
+			setDisplayBox(false);
+			setMessage("Two Factor Auth disabled");
+			setIsVisible(true);
+		}
+		else {
+			setIsActive(true);
+			setDisplayBox(false);
+			setMessage("Two Factor Auth enabled");
+			setIsVisible(true);
+		}
+	}
+
+
+	const [isVisible, setIsVisible] = useState(false);
+	const [message, setMessage] = useState('');
+	useEffect(() => {
+	  if (isVisible) {
+		const timer = setTimeout(() => {
+		  setIsVisible(false);
+		}, 2100);
+
+		return () => clearTimeout(timer);
+	  }
+	}, [isVisible]);
+  
+	return (
+		<div>
+			<CustomBtn id="TwoFAEButton" onClick={handleEnableClick} disable={isActive}>Enable 2FA</CustomBtn>
+			<CustomBtn id="TwoFADButton" onClick={handleDisableClick} disable={!isActive}>Disable 2FA</CustomBtn>
+			{imageUrl !== '' && <div>
+				<img src={imageUrl} height="300" width="300" alt="QR Code" />
+			</div> }
+			{displayBox && <div className="m-4 pt-4">
+				<p className="font-bold text-center">Enter 2FA Code</p>
+				<input type="text" 
+				className="m-2 bg-base border-red  border-0  w-64 h-8 focus:outline-none"
+				value={inputValue}
+				onChange={handleInputChange}/>
+			</div> }
+			<CustomBtn id="codeSubmit" disable={false} onClick={handleSubmit}>Submit</CustomBtn>
+			<div className=" bg-gradient-to-tr from-blue text-base">
+				{isVisible && <p>{message}</p>}
+	  		</div>
+		</div>
+	);
+};
+
+export default TwoFAAuthComponent;
