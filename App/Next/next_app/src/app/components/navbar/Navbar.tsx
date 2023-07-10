209e6b2c2d1b69222e9bb080cf1561550a0b66ea
Clément Paluszek <105011036+Cpaluszek@users.noreply.github.com>
2023-07-10 15:30:08 +0200

    merge navbar components into single file

--- App/Next/next_app/src/app/components/navbar/Navbar.tsx
+++ App/Next/next_app/src/app/components/navbar/Navbar.tsx
@@ -1,14 +1,14 @@
 "use client";
 import Link from "next/link";
 import React from "react";
-import NavLinks from "@/components/navbar/Navlinks";
 import { useLoggedInContext } from '@/context/LoggedInContextProvider';
 
 const Navbar = () => {
+    const {isLoggedIn} = useLoggedInContext();  
     return (
         <nav className="flex items-center flex-wrap justify-between bg-base p-1  drop-shadow-xl">
             <Logo />
-            <NavLinks />
+            {isLoggedIn && <NavLinks /> }
         </nav>
     );
 };
@@ -21,6 +21,14 @@ const Logo = () => {
             <Link href={isLoggedIn ? "/home" : "/"}>
                 Pongolin
             </Link>
+        </div>
+    )
+}
+
+const NavLinks = () => {
+    return (
+        <div className="flex items-center gap-8 px-8 text-xl transition-all">
+            <Link href="/profile" className="text-mauve hover:text-pink">Profile</Link>
         </div>
     )
 }
