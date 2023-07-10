790550d3f262e9a7cc2d659f5bbac0dcf16b0df9
Clément Paluszek <105011036+Cpaluszek@users.noreply.github.com>
2023-07-10 14:43:33 +0200

    fix position of items in channelList

--- App/Next/next_app/src/app/components/chat/ChatMessageBox.tsx
+++ App/Next/next_app/src/app/components/chat/ChatMessageBox.tsx
@@ -1,15 +1,29 @@
 "use client";
+import { useChatContext } from '@/app/context/ChatContextProvider';
 import ChatMessage from './ChatMessage';
 import { MessageModel } from '@/app/utils/models';
+import Image from 'next/image';
 import React from 'react';
+import collapseImg from "../../../../public/collapse-left-svgrepo-com.svg"
 
 const ChatMessagesBox = React.forwardRef<HTMLDivElement, {messages: MessageModel[]}> (({ messages }, ref ) => {
+    const {closeChat} = useChatContext();
     const MessageList = messages.map((message) => (
         <ChatMessage key={message.id} message={message} />
     ))
     return (
-        <div ref={ref} className='overflow-auto h-[84vh]'>
-            {MessageList}
+        <div className='flex flex-col'>
+            <div className='flex flex-row justify-between w-[inherit] mt-2 mb-2 border-b-2 pb-2 border-mantle'>
+                <span className='font-semibold align-middle pl-2'>
+                    Channel Name
+                </span>
+                <button onClick={closeChat} >
+                    <Image src={collapseImg} height={32} width={32} alt="Collapse" className='transition-all' />
+                </button>
+            </div>
+            <div ref={ref} className='overflow-auto h-[80vh]'>
+                {MessageList}
+            </div>
         </div>
     )
 })
--- App/Next/next_app/src/app/components/chat/channel/ChannelList.tsx
+++ App/Next/next_app/src/app/components/chat/channel/ChannelList.tsx
@@ -1,15 +1,14 @@
 import style from '../Chat.module.css';
 import Image from 'next/image';
 import expandImg from "../../../../../public/collapse-right-svgrepo-com.svg"
-import collapseImg from "../../../../../public/collapse-left-svgrepo-com.svg"
 import Channel from "@/components/chat/channel/Channel";
 import AddChannel from "./AddChannel";
 
 import { useChatContext } from '@/app/context/ChatContextProvider';
 import { ChannelModel } from '@/app/utils/models';
+import ShowFriends from '../ShowFriends';
 
 const ChannelList = ( {channels} : {channels: ChannelModel[]}) => {
-    const {isChatOpen, openChat, toggleChatVisibility} = useChatContext();
     const channelsList = channels.map((channel) => (
         <Channel key={channel.id} channel={channel}/>
     ))
@@ -17,10 +16,8 @@ const ChannelList = ( {channels} : {channels: ChannelModel[]}) => {
     return (
         <div className={style.navChannel} >
             <ul className={style.channelContainer}>
-                <button onClick={toggleChatVisibility} >
-                    <Image src={isChatOpen ? collapseImg : expandImg} height={36} width={36} alt="Collapse" className='m-1 transition-all' />
-                </button>
-                <li className="w-full bg-surface1 h-[2px] rounded-sm mb-4 mt-2"></li>
+                <ShowFriends />
+                <li className="w-full bg-surface1 h-[2px] rounded-sm"></li>
                 {channelsList}
                 <AddChannel />
             </ul>
