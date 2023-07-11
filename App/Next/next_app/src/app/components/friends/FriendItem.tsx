be16a33c96c3777b80a66f46debd6a06e29c2904
Clément Paluszek <105011036+Cpaluszek@users.noreply.github.com>
2023-07-11 16:31:31 +0200

    add invite to game and remove friend buttons

--- App/Next/next_app/src/app/components/friends/FriendItem.tsx
+++ App/Next/next_app/src/app/components/friends/FriendItem.tsx
@@ -1,5 +1,7 @@
 import { FriendModel, FriendStatus } from "@/app/utils/models";
 import Image from "next/image";
+import removeFriendImg from "../../../../public/cross-svgrepo-com.svg"
+import playImg from "../../../../public/sword-fill-svgrepo-com.svg"
 
 type FriendProps = {
     friend: FriendModel
@@ -15,16 +17,26 @@ function getColor(status: FriendStatus) {
 
 const FriendItem = ({friend: {username, status, avatar}} : FriendProps) => {
     return (
-        <div className="flex flex-grow relative items-center mt-2 mb-2 hover:bg-surface2 rounded py-1 px-2 mr-2">
-            <div className="relative mr-2 rounded-full w-10 h-10 object-cover">
-                <Image alt="Channel Icon" src={avatar}
-                    height={32} width={32}
-                    className="w-[inherit] rounded-[inherit]" />
-                <div className="absolute bg-base p-[2px] rounded-full -bottom-[1px] -right-[1px]">
-                    <div className={`w-3 h-3 rounded-full ${getColor(status)}`}></div>
+        <div className="flex flex-grow relative items-center justify-between mt-2 mb-2 hover:bg-surface2 rounded py-1 px-2 mr-2">
+            <div className="flex items-center">
+                <div className="relative mr-2 rounded-full w-10 h-10 object-cover">
+                    <Image alt="Channel Icon" src={avatar}
+                        height={32} width={32}
+                        className="w-[inherit] rounded-[inherit]" />
+                    <div className="absolute bg-base p-[2px] rounded-full -bottom-[1px] -right-[1px]">
+                        <div className={`w-3 h-3 rounded-full ${getColor(status)}`}></div>
+                    </div>
                 </div>
+                <h1 className="font-semibold cursor-pointer">{username}</h1>
             </div>
-            <h1 className="font-semibold cursor-pointer">{username}</h1>
+            <div className="flex justify-between gap-3">
+                <button onClick={() => console.log('Invite to game')}>
+                    <Image src={playImg} height={28} width={28} alt="Invite to game" className='transition-all' />
+                </button>
+                <button onClick={() => console.log('remove friend')}>
+                    <Image src={removeFriendImg} height={38} width={38} alt="Invite to game" className='transition-all hover:bg-red' />
+                </button>
+            </div>
         </div>
     )
 }
