d18cd69ecf8b12ef74c913147dd7b3f156877473
Clement Paluszek <cpalusze@z2r1p8.42lyon.fr>
2023-07-17 13:30:23 +0200

    clean file login component

--- App/Next/next_app/src/app/components/Login.tsx
+++ App/Next/next_app/src/app/components/Login.tsx
@@ -1,17 +1,12 @@
 "use client";
-import { useEffect, useState } from "react";
-import CustomBtn from "@/components/CustomBtn";
 
 function LoginComponent() {
 
 	return (
 		<div>
 			<a href="http://localhost:4000/auth/login">Sign With 42</a>
-			{/* <CustomBtn id="" disable={false} onClick={SignIn}>
-			  Sign With 42
-			</CustomBtn> */}
 		</div>
-	  );;
+	 )
 }
 
 export default LoginComponent;
