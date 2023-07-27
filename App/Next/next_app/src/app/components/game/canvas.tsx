2156a013b94b235d9f269841ee43ecf81c258a37
Martin Degraeuwe <mdegraeu@z1r9p5.42lyon.fr>
2023-07-27 18:02:13 +0200

    interfaces player and ball, can draw it but need to pass interfaces to canvas component props

--- App/Next/next_app/src/app/components/game/canvas.tsx
+++ App/Next/next_app/src/app/components/game/canvas.tsx
@@ -1,9 +1,40 @@
 "use client";
+import BallInterface from "@/app/game/interfaces/ballInterface";
+import PlayerInterface from "@/app/game/interfaces/playerInterface";
 import React, {useRef, useEffect, useState } from "react";
 
-export default function Canvas() {
+type CanvasProps = React.DetailedHTMLProps<
+	React.CanvasHTMLAttributes<HTMLCanvasElement>,
+	HTMLCanvasElement
+>
 
+const drawBall = (ball: BallInterface, context: CanvasRenderingContext2D) => {
+		context.fillStyle = ball.color;
+		context.beginPath();
+		context.arc(ball.position[0], ball.position[1], ball.r, 0, ball.pi2, false);
+		context.fill();
+}
+
+const drawPlayer = (player: PlayerInterface, context: CanvasRenderingContext2D) => {
+		context.fillStyle = player.color;
+		context.fillRect(player.position[0] , player.position[1], player.rect[0], player.rect[1]);
+}
+
+const Canvas: React.FC<CanvasProps> = ({ ...props }) => {
+//dessiner chez le client
 	const canvasRef = useRef<HTMLCanvasElement | null>(null);
+
+	const drawPlayerOne = (context: CanvasRenderingContext2D) => {
+		context.fillStyle = 'blue';
+		context.fillRect(10 , 10, 30, 20);
+	}
+
+	const drawBall = (context: CanvasRenderingContext2D) => {
+		context.fillStyle = 'red';
+		context.beginPath();
+		context.arc(200, 200, 10, 0, Math.PI * 2, false);
+		context.fill();
+	}
 	
 	useEffect(() => {
 		const canvas = canvasRef.current;
@@ -12,9 +43,12 @@ export default function Canvas() {
 		const context = canvas.getContext('2d');
 		if (!context)
 			return ;
-		context.fillStyle = 'blue';
-		context.fillRect(10, 10, 30, 20);
+		drawPlayer(props.player1, context);
+		drawPlayer(props.player2, context);
+		drawBall(props.ball, context);
 	}, []);
 
-	return <canvas ref={canvasRef} />
+	return <canvas width={props.width} height={props.height} ref={canvasRef} />
 }
+
+export default Canvas;
--- App/Next/next_app/src/app/game/page.tsx
+++ App/Next/next_app/src/app/game/page.tsx
@@ -1,7 +1,38 @@
 import Canvas from '../components/game/canvas';
+import PlayerInterface from './interfaces/playerInterface';
+import BallInterface from './interfaces/ballInterface';
 
-export default function Game() {
-	return (
-		<Canvas />
-	);
+// window width const
+// window heigth const
+
+const Game = () => {
+// envoyer params
+	let p1: PlayerInterface;
+	let p2: PlayerInterface;
+	let ball: BallInterface;
+	
+	p1 = {
+		color: 'blue',
+		position: [10, 10],
+		rect: [15, 30],
+	}
+
+	p2 = {
+		color: 'blue',
+		position: [190, 10],
+		rect: [15, 30],
+	}
+
+	ball = {
+		color: 'red',
+		position: [100, 100],
+		r: 10, // const
+		pi2: Math.PI * 2, // const
+		direction: [0, 0],
+		speed: [0, 0],
+	}
+
+	return <Canvas width={400} height={400} />;
 }
+
+export default Game;
