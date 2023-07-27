3e3189e5b0422ca91927a49510be0c9601937c05
Clement Paluszek <cpalusze@z3r7p5.42lyon.fr>
2023-07-27 16:07:17 +0200

    update middleware console logs

--- App/Next/next_app/src/middleware.tsx
+++ App/Next/next_app/src/middleware.tsx
@@ -13,14 +13,14 @@ export async function middleware(request: NextRequest) {
     if (verifiedJWT && verifiedJWT.exp && verifiedJWT.exp > currentTime) {
         if (verifiedJWT.twoFactorAuthenticated) {
             if (request.nextUrl.pathname === '/' || request.nextUrl.pathname === '/firstLogin') {
-                console.log('JWT is already valid : Redirecting to /home');
+                console.log('MIDDLEWARE - JWT is already valid : Redirecting to /home');
                 return NextResponse.redirect(new URL('/home', request.url));
             }
         }
     }
     else {
         if (request.nextUrl.pathname !== '/' && request.nextUrl.pathname !== '/firstLogin' && request.nextUrl.pathname !== '/auth/2fa') {
-            console.log(`Not logged in, redirecting to /: ${request.nextUrl.pathname}}`);
+            console.log(`MIDDLEWARE - Not logged in, redirecting to /: ${request.nextUrl.pathname}}`);
             return NextResponse.redirect(new URL( '/', request.url)); 
         }
         return NextResponse.next();
@@ -42,6 +42,6 @@ export const verifyJWT = async (jwtCookie: string) => {
         return verifiedJwt.payload;
     }
     catch (error) {
-        throw new Error(`JWT token is not valid:  + ${error}`);
+        throw new Error(`MIDDLEWARE - JWT token is not valid:  + ${error}`);
     }
 }
