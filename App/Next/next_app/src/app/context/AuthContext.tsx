60f06be5ba28c98f0dada634e1da0c62018d4d6b
Clement <paluszek.clement@gmail.com>
2023-07-28 10:24:37 +0200

    Set interval in AuthContext to refresh jwt

--- App/Next/next_app/src/app/context/AuthContext.tsx
+++ App/Next/next_app/src/app/context/AuthContext.tsx
@@ -22,6 +22,8 @@ const AuthContextDefaultValues: AuthContextType = {
 
 const AuthContext = createContext<AuthContextType>(AuthContextDefaultValues);
 
+const JWT_REFRESH_INTERVAL = 25 * 60 * 1000;
+
 export const AuthContextProvider = ({ children }: { children: React.ReactNode }) => {
     const [isLoggedIn, setLoggedIn] = useState<boolean>(false);
     const [uniqueLogin, setUniqueLogin] = useState<string>("");
@@ -29,23 +31,31 @@ export const AuthContextProvider = ({ children }: { children: React.ReactNode })
 
     // Debug logs
     useEffect(() => {
-        if (uniqueLogin !== "" && userId !== "") {
-            console.log(`uniqueLogin=${uniqueLogin}`);
-            console.log(`userId=${userId}`);
-        }
-    }, [uniqueLogin, userId]);
+        const intervalId = setInterval(() => {
+            console.log("interval triggered: " + Date.now() / 1000);
+            refreshJWT();
+        }, JWT_REFRESH_INTERVAL);
+        return () => clearInterval(intervalId);
+    }, []);
 
 
     const fetchProfile = async () => {
-        const response = await fetch(`${process.env.BACK_URL}/auth/profile`, { credentials: "include" });
-        await response.json().then((data) => {
+        try {
+            const response = await fetch(`${process.env.BACK_URL}/auth/profile`, { credentials: "include" });
+            const data = await response.json();;
             let newLogin: string = data.login as string;
             setUniqueLogin(newLogin);
             let newUserId: string = data.sub as string;
             setUserId(newUserId);
-        }).catch((error) => {
-            throw new Error("Error fetching profile: " + error.message);
-        });
+            if (uniqueLogin !== "" && userId !== "") {
+                console.log(`uniqueLogin=${uniqueLogin}`);
+                console.log(`userId=${userId}`);
+            }
+        }
+        catch (error) {
+            // Todo: logout on error
+            throw new Error("Error fetching profile: " + error);
+        };
     }
 
     const refreshJWT = async () => {
@@ -64,7 +74,6 @@ export const AuthContextProvider = ({ children }: { children: React.ReactNode })
     }
 
     const logout = async () => {
-        console.log("logout");
         await fetch(`${process.env.BACK_URL}/auth/logout`, { credentials: "include" }).catch((error) => {
             console.log("error fetching profile: " + error.message);
         });
