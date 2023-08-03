5a9af395817e888ce85eb65da6c8d9bce731a94e
aurelien <aurelien.calandrino@outlook.fr>
2023-08-03 14:37:30 +0200

    -Grid visual ok
    TODO: responsive height

--- App/Next/next_app/src/app/profile/page.tsx
+++ App/Next/next_app/src/app/profile/page.tsx
@@ -5,8 +5,8 @@ export default function Profile() {
         <div className=" flex flex-full w-full h-full">
             <ChatBar />
             <div className=" py-8 px-10 my-20 mx-12 w-full h-[85vh] grid sm:grid-cols-2 gap-x-36 gap-y-20 rounded">
-                <div className=" rounded-lg bg-surface0 flex hover:shadow-[0_35px_90px_-10px_rgba(0,0,0,0.7)]"></div>
-                <div className="rounded-lg bg-surface0 flex hover:shadow-[0_35px_90px_-10px_rgba(0,0,0,0.7)]"></div>
+                <div className=" transition hover:duration-[550ms] rounded-lg bg-surface0 flex hover:shadow-[0_35px_90px_-10px_rgba(0,0,0,0.7)]"></div>
+                <div className="rounded-lg hover:duration-[550ms] hover:border-4 hover:border-surface1 bg-surface0 flex hover:shadow-[0_35px_90px_-10px_rgba(0,0,0,0.7)]"></div>
             </div>
         </div>
     )
