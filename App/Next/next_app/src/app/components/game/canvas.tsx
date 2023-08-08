b54083a98342cba75fd0aeaf61d61bb54e2a1d24
mdegraeu <martin.degraeuwe@gmail.com>
2023-08-08 14:34:12 +0200

    ball movement and collisions implemented, collisions aren't perfect

--- App/Next/next_app/src/app/components/game/canvas.tsx
+++ App/Next/next_app/src/app/components/game/canvas.tsx
@@ -1,7 +1,5 @@
-"use client";
-import BallInterface from "@/app/game/interfaces/ballInterface";
-import PlayerInterface from "@/app/game/interfaces/playerInterface";
-import React, { useRef, useEffect, useState } from "react";
+// import BallInterface from "@/app/game/interfaces/ballInterface";
+// import PlayerInterface from "@/app/game/interfaces/playerInterface";
 
 // ============================================ // 
 // ===============  TO DO  ==================== // 
@@ -19,39 +17,35 @@ import React, { useRef, useEffect, useState } from "react";
 // add hit box on paddle
 // ============================================ // 
 
+"use client";
+import React, { useRef, useEffect, useState } from "react";
+import Ball from '../../game/class/ball.class';
+import Player from '../../game/class/player.class';
+
 const canvasStyle: any = {
 	backgroundColor: '#009BD7',
 	width: '80%',
 };
 
-const drawBall = (context: CanvasRenderingContext2D, ball: BallInterface, width: number, height: number) => {
-	context.fillStyle = ball.color;
-	context.beginPath();
-		context.arc(ball.position[0] * width - ball.r, ball.position[1] * height - ball.r, ball.r, 0, ball.pi2, false);
-		context.fill();
-	context.closePath();
-}
+// interface CanvasProps {
+// 	ball: Ball;
+// 	renderBall: any;
+// }
 
-const drawPlayer = (context: CanvasRenderingContext2D, player: PlayerInterface, width: number, height: number) => {
-	context.fillStyle = player.color;
-	context.beginPath();
-		context.fillRect(player.position[0] * width - player.rect[0] * width, player.position[1] * height - player.rect[1] * height, player.rect[0] * width, player.rect[1] * height);
-	context.closePath();
-}
-
-const renderGame = (context: CanvasRenderingContext2D , {...props}, width:number, height: number) => {
-
-	context.clearRect(0, 0, width, height);
-	drawPlayer(context, props.player1, width, height);
-	drawPlayer(context, props.player2, width, height);
-	drawBall(context, props.ball, width, height);
-
-}
-
-const Canvas = ({ ...props }) => {
+// const Canvas: React.FC<CanvasProps> = (props) => {
+// const Canvas = ({ ...props }) => {
+// const Canvas = ( props: any ) => {
+const Canvas = () => {
 
 	if (window === undefined)
 		return ;
+	// =================== //
+	// Have To Declrare in //
+	//   parent conponent  //
+		const ball: Ball = new Ball(0.002, 0.004);
+		const p1: Player = new Player(true);
+		const p2: Player = new Player(false);
+	// =================== //
 
 	const [width, setWidth] = useState<number>(window.innerWidth);
 	let height: number;
@@ -59,19 +53,32 @@ const Canvas = ({ ...props }) => {
 	const canvasRef = useRef<HTMLCanvasElement>(null);
 
 	useEffect(() => {
-			const canvas = canvasRef.current;
-			if (!canvas)
-				return ;
-			const context = canvas.getContext('2d');
-			if (!context)
-				return ;
-			height = width * (9 / 16);
+		const canvas = canvasRef.current;
+		if (!canvas)
+			return ;
+		const context = canvas.getContext('2d');
+		if (!context)
+			return ;
+		height = width * (9 / 16);
+		
+		let animationId: number;
+		
+		const render = (): any => {
 
-			// ======================== //
-			// Do we want to loose canvas in height
-			// or to play in a stamp
+			context.clearRect(0, 0, width, height);
+			ball.renderBall(context, p1, p2, width, height);
+			p1.renderPlayer(context, width, height);
+			p2.renderPlayer(context, width, height);
 
-			renderGame(context, props, width, height);
+			animationId = window.requestAnimationFrame(render);
+		}
+		render();
+		
+		return () => window.cancelAnimationFrame(animationId);
+
+		// ======================== //
+		// Do we want to loose canvas in height
+		// or to play in a stamp
 	}, [width]);
 
 	useEffect(() => {
---
+++ App/Next/next_app/src/app/game/class/ball.class.tsx
@@ -0,0 +1,73 @@
+import Player from './player.class';
+
+class Ball {
+    constructor(xspeed: number, yspeed: number) {
+        let val = 1;
+        if (Math.random() < 0.5) {
+           val *= -1;
+        }
+        this.speed[0] = xspeed * val;
+        this.speed[1] = yspeed * val;
+    }
+
+    color: string = 'red';
+    x: number = 0.5;
+    y: number = 0.5;
+    r: number = 0.1;
+    pi2: number = Math.PI * 2;
+    speed: [number, number] = [0, 0];
+
+    borderBounce(p1: Player, p2: Player, width:  number, height: number) {
+        if (this.x <= 0) 
+            this.speed[0] *= -1; // reset and count p2 score ++
+        if(this.x >= 1)
+            this.speed[0] *= -1; // reset and count p1 score ++
+        if (this.y - this.r / 2 <= 0 || this.y >= 1)
+            this.speed[1] *= -1;
+        this.paddleBounce(p1, p2);
+    };
+
+    paddleBounce(p1: Player, p2: Player) {
+        if (this.speed[0] < 0)
+            this.paddleLeftBounce(p1);
+        else
+            this.paddleRightBounce(p2);
+    }
+
+    paddleLeftBounce(p: Player) {
+        if (this.x - this.r / 2 < p.x + p.w) {
+            if ( this.y > p.y && this.y < p.y + p.h) {
+                this.speed[0] *= -1;
+            }
+        }
+    };
+
+    paddleRightBounce(p: Player) {
+        if (this.x > p.x) {
+            if (this.y > p.y && this.y < p.y + p.h) {
+                this.speed[0] *= -1;
+            }
+        }
+    };
+
+    update() {
+        this.x += this.speed[0];
+        this.y += this.speed[1];
+    };
+
+    show(context: CanvasRenderingContext2D, width:  number, height: number) {
+        context.fillStyle = this.color;
+        context.beginPath();
+            context.arc(this.x * width - this.r * 100, this.y * height - this.r * 100, this.r * 100, 0, this.pi2, false);
+            context.fill();
+        context.closePath();
+    };
+
+    renderBall(context: CanvasRenderingContext2D, p1: Player, p2: Player, width: number, height: number) {
+        this.show(context, width, height);
+        this.borderBounce(p1, p2, width, height);
+        this.update();
+    };
+}
+
+export default Ball;
