acafc3a2d6e2f32f1d2b0bf4c72171abba51b33b
aucaland <aurelien.calandrino@outlook.fr>
2023-08-08 11:27:51 +0200

    task suspended, had to create a Avatar Component

--- App/Next/next_app/src/app/profile/page.tsx
+++ App/Next/next_app/src/app/profile/page.tsx
@@ -7,10 +7,10 @@ export default function Profile() {
         <div className=" flex flex-row w-full h-full">
             <ChatBar />
             <div className="flex flex-col w-full h-full mx-5">
-                <div className=" font-semibold text-gray-400 text-center h-[15vh] mt-3 transition hover:duration-[550ms] rounded-lg bg-surface0 hover:shadow-[0_35px_55px_-20px_rgba(0,0,0,0.7)]">
+                <div className=" mx-20 font-semibold text-gray-400 text-center h-[20vh] mt-5 transition hover:duration-[550ms] rounded-lg bg-surface0 hover:shadow-[0_35px_55px_-20px_rgba(0,0,0,0.7)]">
                     Stats <br /><br /><br /> Here Stats Component(s) will be rendered
                 </div>
-                <div className=" h-[72vh] mt-6 rounded-lg transition hover:duration-[550ms] bg-surface0  hover:shadow-[0_35px_55px_-20px_rgba(0,0,0,0.7)]">
+                <div className=" h-[70vh] mx-10 my-5 rounded-lg transition hover:duration-[550ms] bg-surface0  hover:shadow-[0_35px_55px_-20px_rgba(0,0,0,0.7)]">
                     <UnderlineTabs />
                 </div>
                 </div>
