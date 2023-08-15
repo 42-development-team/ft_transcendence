4b4e37124739f1588a069000484b1fd9736157f5
Cpaluszek <paluszek.clement@gmail.com>
2023-08-15 14:04:48 +0200

    Chat scrolling OK

--- App/Next/next_app/src/app/hooks/useChatScrolling.tsx
+++ App/Next/next_app/src/app/hooks/useChatScrolling.tsx
@@ -1,21 +1,23 @@
-import { useCallback, useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from "react";
 import { MessageModel } from "../utils/models";
 
-export default function useChatLiveScrolling<T extends HTMLElement>(
+export default function useChatScrolling<T extends HTMLElement>(
     messages: MessageModel[]
 ) {
     const chatMessageBoxRef = useRef<T | null>(null);
 
-    const scrollNewMessages = useCallback(() => {
-        chatMessageBoxRef.current?.lastElementChild?.scrollIntoView()
-    }, [])
+    const scrollNewMessages = () => {
+        chatMessageBoxRef.current?.lastElementChild?.scrollIntoView(
+            { behavior: "smooth" }
+        )
+    }
 
     useEffect(() => {
-            scrollNewMessages()
+        console.log("useChatScrolling: useEffect: messages changed");
+        scrollNewMessages();
     }, [messages, scrollNewMessages])
 
     return {
-        chatMessageBoxRef,
-        scrollNewMessages,
+        chatMessageBoxRef
     }
 }
