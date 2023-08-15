72c5fd723ee2d79332d4790e3342ab8df3a6f6c7
aurelien <aurelien.calandrino@outlook.fr>
2023-08-15 16:14:06 +0200

    add line separation between stats elenments

--- App/Next/next_app/src/app/components/profile/Stats.tsx
+++ App/Next/next_app/src/app/components/profile/Stats.tsx
@@ -9,14 +9,23 @@ const Stats = ( userId: {userId : string }) => {
 						<div className="text-center">Played</div>
 						<div className="">15</div>
 					</div>
+					<div className="flex flex-row justify-center">
+						<div className="flex flex-middle w-[5vw] border border-gray-600"></div>
+					</div>
 					<div className="flex flex-col py-[1vw] h-full w-full justify-center text-center">
 						W/L/D-R%
 						<div className="">10/5-1.5</div>
 					</div>
 				</div>
+				<div className="flex flex-col justify-center">
+					<div className="flex flex-middle h-[105px] border border-gray-600 mx-2"></div>
+				</div>
 				<div className="flex flex-col w-full h-full">
 					<div className="flex py-[1vw] justify-center w-full h-full">
 						Some component
+					</div>
+					<div className="flex flex-row justify-center">
+						<div className="flex flex-middle w-[5vw] border border-gray-600"></div>
 					</div>
 					<div className="flex py-[1vw] justify-center w-full h-full">
 						Some component
