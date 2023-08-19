402965eec3d599f5c1a98f40817dd70e76ce303a
Cpaluszek <paluszek.clement@gmail.com>
2023-08-19 13:14:05 +0200

    Extract chatMessageBox header to its own component

---
+++ App/Next/next_app/src/app/components/chat/chatbox/ChatHeader.tsx
@@ -0,0 +1,24 @@
+import Image from 'next/image';
+import collapseImg from "../../../../../public/collapse-left-svgrepo-com.svg"
+
+type ChatHeaderProps = {
+    title: string,
+    onCollapse: () => void,
+    children?: React.ReactNode
+}
+
+const ChatHeader = ({title, onCollapse, children}: ChatHeaderProps) => {
+    return (
+        <div className='flex flex-row justify-between border-b-2 pb-2 border-mantle'>
+            {children}
+            <span className='font-semibold align-middle pt-2 pr-2'>
+                {title}
+            </span>
+            <button onClick={onCollapse} >
+                <Image src={collapseImg} height={32} width={32} alt="Collapse" className='transition-all' />
+            </button>
+        </div>
+    )
+}
+
+export default ChatHeader;
