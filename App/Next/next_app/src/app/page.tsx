7d17cda7698835afaf4970a27faebfff7d85cc3c
Cpaluszek <paluszek.clement@gmail.com>
2023-08-19 10:20:22 +0200

    remove comments from login page

--- App/Next/next_app/src/app/page.tsx
+++ App/Next/next_app/src/app/page.tsx
@@ -1,19 +1,10 @@
 import './globals.css';
-// import {useLoggedInContext} from "@/context/LoggedInContextProvider";
-// import CustomBtn from "@/components/CustomBtn";
 import LoginComponent from "@/components/Login";
 
 export default function Home() {
-  // const {isLoggedIn, login, logout} = useLoggedInContext();
   return (
       <div className="flex flex-col flex-auto items-center justify-center">
           <LoginComponent />
-          {/* <CustomBtn id="" disable={false} onClick={ () => {
-              isLoggedIn ? logout() : login("toto");
-              console.log(`Log status: ${isLoggedIn}`)
-            }}>
-            {isLoggedIn ? "<Test> Logout" : "<Test> Login" }
-          </CustomBtn> */}
       </div>
   )
 }
