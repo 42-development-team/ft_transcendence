f9c35d2ea1b71d0686071e593d5b2ba248f8a41d
Cpaluszek <paluszek.clement@gmail.com>
2023-08-19 14:31:07 +0200

    Add material-tailwind tooltip to buttons

--- App/Next/next_app/src/app/components/chat/chatbox/ChatHeader.tsx
+++ App/Next/next_app/src/app/components/chat/chatbox/ChatHeader.tsx
@@ -1,3 +1,4 @@
+import { Tooltip } from '@material-tailwind/react';
 import Image from 'next/image';
 import collapseImg from "../../../../../public/collapse-left-svgrepo-com.svg"
 
@@ -15,9 +16,11 @@ const ChatHeader = ({title, onCollapse, children}: ChatHeaderProps) => {
             <span className='font-semibold align-middle pt-2 pr-2'>
                 {title}
             </span>
-            <button onClick={onCollapse} >
-                <Image src={collapseImg} height={32} width={32} alt="Collapse" className='transition-all' />
-            </button>
+            <Tooltip placement="bottom-end" content="Collapse" className="tooltip" >
+                <button onClick={onCollapse} >
+                    <Image src={collapseImg} height={32} width={32} alt="Collapse" className='transition-all' />
+                </button>
+            </Tooltip>
         </div>
     )
 }
