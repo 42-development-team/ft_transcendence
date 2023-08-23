bff844588da10d51120a5d9b04188e653af3c8ae
Cpaluszek <paluszek.clement@gmail.com>
2023-08-23 13:03:35 +0200

    Update JWT expiration time to 1 day

--- App/Nest/nest_app/src/auth/auth.service.ts
+++ App/Nest/nest_app/src/auth/auth.service.ts
@@ -88,7 +88,7 @@ export class AuthService {
             const [at, rt] = await Promise.all([
                 this.jwtService.signAsync(jwtPayload, {
                     secret: this.configService.get<string>('jwtSecret'),
-                expiresIn: "30m",
+                expiresIn: "1d",
             }),
                 this.jwtService.signAsync(jwtPayload, {
                     secret: this.configService.get<string>('jwtRefreshSecret'),
