b192985324389daeeb15afc642c6c040f3b40b37
Aurelien calandrino <aurelien.calandrino@outlook.fr>
2023-08-23 12:45:27 +0200

    improve match history visual

--- App/Next/next_app/src/app/components/profile/matchHistory.tsx
+++ App/Next/next_app/src/app/components/profile/matchHistory.tsx
@@ -4,11 +4,23 @@ const matchHistory = ( props: { data: any } ) => {
 
     return (
         <div className="p-6 h-[50vh] overflow-auto">
+            <div className="flex flex-col">
             {data.map((item: any, index: number) => (
-                <div key={index} className="h-20 m-2 border-b border-gray-300 rounded-none text-xl font-bold">
-                    {item}
-                </div>
+                <div key={index} className="bg-gradient-to-r from-green-600 to-base flex flex-row justify-between h-20 m-2 my-4 text-xl font-bold">
+                    <span className="flex flex-grow justify-between bg-gradient-to-r from-base to-surface0 p-2 block m-1">
+                    <div className="flex flex-col justify-center">
+                            {item}
+                    </div>
+                    <div className="flex flex-col justify-center">
+                        {item}
+                    </div>
+                    <div className="flex flex-col justify-center">
+                        {item}
+                    </div>
+                    </span>
+                    </div>
             ))}
+            </div>
         </div>
     )
 }
