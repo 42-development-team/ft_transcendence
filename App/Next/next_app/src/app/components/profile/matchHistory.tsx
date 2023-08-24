d78234e2b401c31266aa9e9a94d17945aba26814
aurelien <aurelien.calandrino@outlook.fr>
2023-08-24 09:46:19 +0200

    match history element are now yellow on win and grey on lose

--- App/Next/next_app/src/app/components/profile/matchHistory.tsx
+++ App/Next/next_app/src/app/components/profile/matchHistory.tsx
@@ -6,9 +6,10 @@ const matchHistory = ( props: { data: any } ) => {
         <div className="p-6 h-[50vh] overflow-auto">
             <div className="flex flex-col">
             {data.map((item: any, index: number) => (
-                <div key={index} className="p-2 bg-gradient-to-r from-yellow-500 to-base border-l-2 flex flex-row justify-between h-[120px] m-2 my-4 text-xl font-bold">
+                <div key={index} className={item.win === true ? 'rounded pl-1 pb-1 flex flex-row justify-between h-[120px] m-2 my-4 text-xl font-bold bg-gradient-to-r from-yellow-500 to-base' 
+                    : 'rounded pl-1 pb-1 flex flex-row justify-between h-[120px] m-2 my-4 text-xl font-bold bg-gradient-to-r from-gray-500 to-gray'}>
                     <span className="flex flex-grow justify-between bg-gradient-to-r from-base to-surface0 px-2">
-                        <div className={`flex flex-col justify-center pl-4`} style={{ color: item.win === true ? "orange" : "gray"}}>
+                        <div className={`flex flex-col justify-center pl-4`} style={{ color: item.win === true ? "orange" : "grey"}}>
                             {item.win.toString()}
                         </div>
                         <div className="flex flex-col justify-center">
