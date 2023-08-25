cc17dfa2e42c108d1204529db4994da34298f18d
Aurelien calandrino <aurelien.calandrino@outlook.fr>
2023-08-25 10:52:03 +0200

    enhance visual, get some color accordingly to the catpuccin theme

--- App/Next/next_app/src/app/components/profile/matchHistory.tsx
+++ App/Next/next_app/src/app/components/profile/matchHistory.tsx
@@ -21,14 +21,14 @@ const matchHistory = ( props: { data: any, currentUserId: number } ) => {
         <div className="p-6 h-[50vh] overflow-auto">
             <div className="flex flex-col">
                 {data.map((item: any, index: number) => (
-                    <div key={index} className={item.winner.id === currentUserId ? 'rounded pl-1 pb-1 flex flex-row justify-between h-[120px] m-2 my-4 text-xl font-bold bg-gradient-to-r from-yellow-500 to-base'
+                    <div key={index} className={item.winner.id === currentUserId ? 'rounded pl-1 pb-1 flex flex-row justify-between h-[120px] m-2 my-4 text-xl font-bold bg-gradient-to-r from-peach to-base'
                         : 'rounded pl-1 pb-1 flex flex-row justify-between h-[120px] m-2 my-4 text-xl font-bold bg-gradient-to-r from-gray-500 to-gray'}>
                         <span className=" flex flex-grow justify-between bg-gradient-to-r from-base to-surface0 px-2">
-                            <div className={`flex flex-col justify-center pl-4`} style={{ color: item.winner.id === currentUserId ? "orange" : "grey" }}>
+                            <div className={`flex flex-col justify-center pl-[6vw] text-2xl`} style={{ color: item.winner.id === currentUserId ? "#fab387" : "grey" }}>
                                 {item.winner.id === currentUserId ? "Win" : "Lose"}
                             </div>
                             <div className="flex flex-col justify-center">
-                                <div className="flex justify-center text-red-800">
+                                <div className="flex justify-center text-maroon">
                                     VS
                                 </div>
                                 <button onClick={() => onProfileClick(item.winner.id === currentUserId ? item.loser.id : item.winner.id)} className="flex flex-col justify-center hover:scale-110 hover:text-gray-300 ">
@@ -36,11 +36,14 @@ const matchHistory = ( props: { data: any, currentUserId: number } ) => {
                                 </button>
                             </div>
                             <div className="flex flex-col justify-center">
-                                <div className="flex justify-center">
+                                <div className="flex justify-center text-2xl" style={{ color: item.winner.id === currentUserId ? "#fab387" : "grey" }}>
                                     {item.winner.id === currentUserId ? item.winnerScore : item.loserScore} - {item.winner.id === currentUserId ? item.loserScore : item.winnerScore}
                                 </div>
-                                <div>
+                                <div className="flex justify-center">
                                     {item.createdAt.slice(0, 10)}
+                                </div>
+                                <div className="flex justify-center">
+                                    {item.createdAt.slice(11, 19)}
                                 </div>
                             </div>
                         </span>
