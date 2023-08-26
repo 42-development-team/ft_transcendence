1af04b5efef155d6daba5d46296ba571a1e4f4ac
aurelien <aurelien.calandrino@outlook.fr>
2023-08-26 15:50:39 +0200

    visual is now much better; TODO: cancel button

--- App/Next/next_app/src/app/components/CustomBtnPlay.tsx
+++ App/Next/next_app/src/app/components/CustomBtnPlay.tsx
@@ -5,6 +5,8 @@ const CustomBtn = (
             color="bg-mauve", 
             disable=false,
             anim=true,
+			height=120,
+			width=240,
         } 
             : 
         {
@@ -14,6 +16,8 @@ const CustomBtn = (
             color?: string, 
             disable: boolean,
             anim?: boolean
+			height?: number,
+			width?: number,
         }
     ) => {
     return (
@@ -22,8 +26,9 @@ const CustomBtn = (
             id={id}
             disabled={disable}
             style={{opacity: disable? 0.5 : 1}}
-            className={` ${anim && ' text-[2rem] focus:ring-4 shadow-lg transform active:scale-75 transition-transform'} ${disable && 'bg-gradient-to-br from-mauve via-teal to-mauve'}
-			h-[120px] w-[240px] font-bold text-2xl rounded-lg text-base ${color} disabled:pointer-events-none 
+            className={` ${anim && ' text-[2rem] focus:ring-4 shadow-lg transform active:scale-75 transition-transform'} 
+			${disable && 'bg-transparent'}
+			h-[${height}px] w-[${width}px] font-bold text-2xl rounded-lg text-base ${color} disabled:pointer-events-none 
 			hover:bg-pink drop-shadow-xl m-4 p-3 flex justify-center items-center`}
             onClick={onClick}>
             {children}
--- App/Next/next_app/src/app/components/home/play.tsx
+++ App/Next/next_app/src/app/components/home/play.tsx
@@ -1,6 +1,7 @@
 import { Spinner } from '@material-tailwind/react'
 import { useState } from 'react'
 import CustomBtnPlay from '../CustomBtnPlay'
+import CustomBtn from '../CustomBtn'
 
 const Play = () => {
 
@@ -11,26 +12,43 @@ const Play = () => {
   const matchmaking = async () => {
 	setLoading(true)
 	setDisable(true)
-	setButtonText("")
+	setButtonText("Calling racket master...")
 	//TODO: handle matchmaking
-
   }
 
-	return (
-	<div className='flex flex-row '>
-		<div className='flex flex-col justify-center items-center'>
-			<CustomBtnPlay
-				disable={disable}
-				onClick={matchmaking}>
-					{buttonText}
-					{loading &&
-					<div className='flex flex-row justify-center shapes-5 ' style={{opacity: 1}}>
+  const cancelMatchmaking = async () => {
+	setLoading(false)
+	}
 
-					</div>}
-			</CustomBtnPlay>
+	return (
+		<div className='flex flex-col '>
+			<div className='flex flex-col justify-center items-center'>
+				{loading ? (
+					<div className='flex flex-col'>
+						<div className='flex flex-row justify-center'>
+							<div className='flex shapes-5 ' style={{ opacity: 1 }}></div>
+						</div>
+							<div className='flex text-center text-[1.4rem] mt-6 italic font-extralight'>{buttonText}</div>
+					</div>
+				) : (
+					<CustomBtnPlay
+						disable={disable}
+						onClick={matchmaking}>
+						{!loading && buttonText}
+					</CustomBtnPlay>
+				)}
+			</div>
+			{loading &&
+				<div className='flex flex-row justify-center'>
+					<div className='flex '>
+					<CustomBtnPlay onClick={cancelMatchmaking} width={110} height={60} color='bg-red-500' disable={false} anim={false}>
+						X
+					</CustomBtnPlay>
+					</div>
+				</div>
+			}
 		</div>
-	</div>
-  )
+	)
 }
 
 export default Play
