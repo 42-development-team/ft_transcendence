e84b2cac9d3c89265c1ad6120ca7a59b5e4f0210
Cpaluszek <paluszek.clement@gmail.com>
2023-08-27 15:45:30 +0200

    ChatMemberList username color depending on role

--- App/Next/next_app/src/app/components/chat/chatbox/members/ChatMemberItem.tsx
+++ App/Next/next_app/src/app/components/chat/chatbox/members/ChatMemberItem.tsx
@@ -73,6 +73,18 @@ const ChatMemberItem = ({
         removeAdmin(id);
     }
 
+    const getColor = () => {
+        if (isOwner) {
+            return 'text-[#f38ba8]';
+            // return '#fab387';
+        } else if (isAdmin) {
+            return 'text-[#c6a0f6]';
+        } else if (isBanned) {
+            return 'text-[#838ba7]';
+        }
+        return 'text-[#f5e0dc]';
+    }
+
     return (
         <div className={` ${isCurrentUser && "bg-surface0"}  flex flex-grow relative items-center justify-between mt-2 mb-2 hover:bg-surface1 rounded py-1 px-2 mr-2`}>
             <div className="flex items-center">
@@ -86,7 +98,7 @@ const ChatMemberItem = ({
                         <div className={`w-3 h-3 rounded-full ${getStatusColor(userStatus)}`}></div>
                     </div>
                 </div>
-                <h1 className={`${isCurrentUser && "text-peach font-semibold"} pl-[0.15rem]`}>{username}</h1>
+                <h1 className={`${getColor()} pl[0.15rem] ${isCurrentUser && 'font-semibold'}`}>{username}</h1>
             </div>
             <ChatMemberActions isCurrentUser={isCurrentUser} isMemberAdmin={isAdmin} isMemberOwner={isOwner} isBanned={isBanned} userId={id} 
                 kickUser={kickUser} banUser={banUser} leaveChannel={leaveChannel} 
