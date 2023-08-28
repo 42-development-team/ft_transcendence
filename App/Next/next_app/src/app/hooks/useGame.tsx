0d54f12f34b49e04f47eef9e27c6cdf588c91a86
Martin Degraeuwe <mdegraeu@z1r9p5.42lyon.fr>
2023-08-28 15:56:01 +0200

    main merged and game datas init in socketGateway

--- App/Next/next_app/src/app/hooks/useGame.tsx
+++ App/Next/next_app/src/app/hooks/useGame.tsx
@@ -11,47 +11,6 @@ export default function useGame() {
 
 	const handleNewGameConnection = (body: any) => {
 		const { room, user } = body;
-		let player1: PlayerInterface = {
-			name: user.username,
-			color: '#cba6f7aa',
-    		x: 0.02,
-    		y: 0.5,
-    		w: 0.01,
-    		h: 0.15,
-    		velocity: 0,
-    		angle: 60,
-    		points: 0,
-		}
-
-		let player2: PlayerInterface = {
-			name: user.username,
-			color: '#cba6f7aa',
-    		x: 0.98,
-    		y: 0.5,
-    		w: 0.01,
-    		h: 0.15,
-    		velocity: 0,
-    		angle: 60,
-    		points: 0,
-		}
-
-		let ball: BallInterface = {
-			color: '#cba6f7',
-    		x: 0.5,
-    		y: 0.5,
-    		r: 0.01,
-    		pi2: Math.PI * 2,
-    		speed: [0, 0],
-    		incr: 0,
-		}
-
-		let data: GameInterface = {
-			player1: player1,
-			player2: player2,
-			ball: ball,
-		}
-
-		return data;
 	};
 
 	// init data with p1, p2 and ball infos
