e3b1ba7f65229578e6b016f0211e8fcfa77750f3
Cpaluszek <paluszek.clement@gmail.com>
2023-08-30 11:28:40 +0200

    Add tailwind material tooltip to channel button

--- App/Next/next_app/src/app/components/chat/channel/Channel.tsx
+++ App/Next/next_app/src/app/components/chat/channel/Channel.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import { useChatBarContext } from '@/app/context/ChatBarContextProvider';
 import { ChannelModel, ChannelType } from '@/app/utils/models';
 import { useEffect } from 'react';
+import { Tooltip } from '@material-tailwind/react';
 
 type ChannelProps = {
     channel: ChannelModel
@@ -23,25 +24,27 @@ const Channel = ({channel :{name, icon, type, id, unreadMessages}}: ChannelProps
 
     return (
         <li className={styles.channelItem}>
-            <button onClick={handleClick} 
-                className={`
-                    ${type == ChannelType.Public && "bg-blue text-base"} 
-                    ${type == ChannelType.Protected && "bg-yellow text-base"}
-                    ${type == ChannelType.DirectMessage && "bg-green text-base"}
-                    rounded-[inherit] w-[inherit] h-[inherit] relative`}>
-                {icon === '' ?
-                    <p className='text-xs'>{name}</p> :
-                    <Image alt="Channel Icon" fill src={icon} 
-                    sizes=" 100vw, 100vw"
-                    className="rounded-[inherit]" />
+            <Tooltip placement="right" content={name} className="tooltip" offset={8}>
+                <button onClick={handleClick} 
+                    className={`
+                        ${type == ChannelType.Public && "bg-blue text-base"} 
+                        ${type == ChannelType.Protected && "bg-yellow text-base"}
+                        ${type == ChannelType.DirectMessage && "bg-green text-base"}
+                        rounded-[inherit] w-[inherit] h-[inherit] relative`}>
+                    {icon === '' ?
+                        <p className='text-xs'>{name}</p> :
+                        <Image alt="Channel Icon" fill src={icon} 
+                        sizes=" 100vw, 100vw"
+                        className="rounded-[inherit]" />
+                    }
+                {unreadMessages > 0 && 
+                    <div className="absolute inline-flex items-center justify-center w-6 h-6 text-xs font-bold 
+                        text-text bg-rose-500 border-2 border-base rounded-full -top-2 -right-2">
+                        {unreadMessages}
+                    </div>
                 }
-            {unreadMessages > 0 && 
-                <div className="absolute inline-flex items-center justify-center w-6 h-6 text-xs font-bold 
-                    text-text bg-rose-500 border-2 border-base rounded-full -top-2 -right-2">
-                    {unreadMessages}
-                </div>
-            }
-            </button>
+                </button>
+            </Tooltip>
         </li>
     );
 }
