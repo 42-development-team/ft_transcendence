8dab16b0d7753fec56ab53ac05f1315d8b9dd67e
bl000m <mathiapagani@gmail.com>
2023-08-31 12:40:51 +0200

    fix bordet top transparent no more

--- App/Next/next_app/src/app/components/chat/chatbox/members/ChatMemberList.tsx
+++ App/Next/next_app/src/app/components/chat/chatbox/members/ChatMemberList.tsx
@@ -198,10 +198,10 @@ const ChatMemberList = ({ channel, userId, directMessage }: ChatMemberListProps)
     const InviteFieldButton = () => {
 		const { updateChatBarState } = useChatBarContext();
 		return (
-				<div className="relative flex h-10 w-full min-w-[200px] max-w-[24rem]">
+				<div className="relative flex h-10 w-full">
 					<input
 						type="email"
-						className="peer h-full w-full rounded-[7px] border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-2.5 pr-20 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-pink-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
+						className="peer h-full w-full rounded-[7px] border border-blue-gray-200 bg-transparent px-3 py-2.5 pr-20 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-pink-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
 						placeholder=" "
 						required
 					/>
