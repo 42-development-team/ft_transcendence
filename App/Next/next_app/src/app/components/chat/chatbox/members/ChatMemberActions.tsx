59d31eeedbe08d25aa294c678090c15f64d72e07
Cpaluszek <paluszek.clement@gmail.com>
2023-09-01 11:11:05 +0200

    Change mute button appearance is user is muted

--- App/Next/next_app/src/app/components/chat/chatbox/members/ChatMemberActions.tsx
+++ App/Next/next_app/src/app/components/chat/chatbox/members/ChatMemberActions.tsx
@@ -60,10 +60,10 @@ const ChatMemberActions = (
                 <div ref={wrapperRef} className=" text-left w-full">
                     <Tooltip content="Mute" placement="top" className="tooltip">
                         <button onClick={() => setIsOpen(!isOpen)}
-                            className="inline-flex justify-center w-full rounded-2xl px-2 py-2 bg-base">
+                            className={`inline-flex justify-center w-full rounded-2xl px-2 py-2 bg-base ${isMuted && " bg-crust"}`}>
                             <svg className="w-6 h-6" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M12 7V12L14.5 13.5M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z"
-                                    stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path>
+                                    stroke={isMuted ? "#f9e2af" : "currentColor"} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path>
                             </svg>
                         </button>
                     </Tooltip>
