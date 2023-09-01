a5a91c50cad4a075deb304168fecd5345f1416e9
Aurelien calandrino <aurelien.calandrino@outlook.fr>
2023-09-01 10:10:11 +0200

    localStorage in progress for theme

--- App/Next/next_app/src/app/components/theme/Theme.tsx
+++ App/Next/next_app/src/app/components/theme/Theme.tsx
@@ -1,19 +1,22 @@
 "use client";
+
 import { useState } from 'react';
 
-export const Theme = ({children}: {children: React.ReactNode}) => {
-    const [theme, setTheme] = useState("mocha");
-    const toggleTheme = () => {
-  setTheme((currentTheme) => currentTheme === "mocha" ? "latte" : "mocha");
-    };
+export const Theme = ({ children }: { children: React.ReactNode }) => {
+  const [theme, setTheme] = useState("mocha");
+  const toggleTheme = () => {
+    
+    setTheme((currentTheme) => currentTheme === "mocha" ? "latte" : "mocha");
+    
+  };
 
   return (
     <body className={`${theme}`}>
-      <div className='fixed z-10'>
+      <div className='fixed z-10 '>
         <button onClick={toggleTheme}>Switch Theme</button>
-        </div>
-        {children}
+      </div>
+      {children}
     </body>
-    );
+  );
 };
 
