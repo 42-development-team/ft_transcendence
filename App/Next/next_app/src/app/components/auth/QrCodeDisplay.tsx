e8a62e75d8db2f5beddfd8adde8bc2606528ce0e
Aurelien calandrino <aurelien.calandrino@outlook.fr>
2023-09-02 16:44:53 +0200

    remove shadow on QRCode, reduce shadow on 2fa window

--- App/Next/next_app/src/app/components/auth/QrCodeDisplay.tsx
+++ App/Next/next_app/src/app/components/auth/QrCodeDisplay.tsx
@@ -4,7 +4,7 @@ const QrCodeDisplay = ({children, imageUrl="", displayBox=false} : {children: an
     return (
         imageUrl !== '' &&  displayBox && 
         <div className=" flex justify-center">
-            <img className=" rounded shadow-[0_30px_60px_-10px_rgba(0,0,0,0.69)] border-2 border-surface0" 
+            <img className=" rounded shadow-[0_30px_60px_-10px_rgba(0,0,0,0.05)] border-2 border-surface0" 
             placeholder={'/home/aurel/Documents/ft_transcendence/App/Next/next_app/public/logout-svgrepo-com.svg'} //TODO: add placeHolder accordingly
             src={imageUrl} 
             height="150" 
