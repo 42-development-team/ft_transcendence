1548db9674b032e68c6f13c3f24d38255ec0ce84
Cpaluszek <paluszek.clement@gmail.com>
2023-09-02 14:14:06 +0200

    setttings page refesh OK + add chat interface

--- App/Next/next_app/src/app/settings/page.tsx
+++ App/Next/next_app/src/app/settings/page.tsx
@@ -1,8 +1,8 @@
-
 import TwoFA from "../components/auth/TwoFA";
 import getJwt from '@/app/utils/getJwt';
 import { useRouter } from "next/navigation";
 import SettingsPage from "../components/settings/settingsPage";
+import Chat from "../components/chat/Chat";
 
 export default async function Settings() {
 
@@ -16,11 +16,14 @@ export default async function Settings() {
     const userId = payload.sub;
 
     return (
-        <div className="flex flex-col w-full">
-            <SettingsPage userId={userId}></SettingsPage>
-            <div className="flex flex-row justify-center">
-                <TwoFA userId={userId}></TwoFA>
-            </div>
+        <div className="flex flex-auto w-full h-full">
+			<Chat userId={userId}/>
+				<div className="flex flex-col w-full h-[calc(100vh-48px)] justify-center">
+				<SettingsPage userId={userId}></SettingsPage>
+				<div className="flex flex-row justify-center">
+					<TwoFA userId={userId}></TwoFA>
+				</div>
+			</div>
         </div>
     )
 }
