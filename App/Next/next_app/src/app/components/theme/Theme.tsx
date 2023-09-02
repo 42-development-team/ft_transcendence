e5a84e8f1ca4b3a20155785662c7d6d2c4d39961
Aurelien calandrino <aurelien.calandrino@outlook.fr>
2023-09-02 15:34:26 +0200

    theme are now loading correctly with logo, switch button is buggy on light to  dark

--- App/Next/next_app/src/app/components/theme/Theme.tsx
+++ App/Next/next_app/src/app/components/theme/Theme.tsx
@@ -5,7 +5,7 @@ import sun from "../../../../public/sun.png";
 import sunLight from "../../../../public/sunLight.png";
 
 export const Theme = ({ children }: { children: React.ReactNode }) => {
-  const [theme, setTheme] = useState("mocha");
+  const [theme, setTheme] = useState(localStorage.getItem("theme") || "mocha");
   const [position, setPosition] = useState("left");
 
   const toggleTheme = () => {
@@ -27,7 +27,6 @@ export const Theme = ({ children }: { children: React.ReactNode }) => {
   
   useEffect(() => {
     setTheme(localStorage.getItem("theme") || "mocha");
-    localStorage.getItem("theme") === "mocha" || null ? setPosition("left") : setPosition("right");
   }, []);
 
   return (
@@ -41,15 +40,27 @@ export const Theme = ({ children }: { children: React.ReactNode }) => {
                 className="peer absolute h-4 w-8 cursor-pointer appearance-none rounded-full bg-overlay0 transition-colors duration-300 checked:bg-pink-500 peer-checked:border-pink-500 peer-checked:before:bg-pink-500"
                 onClick={toggleTheme}
               />
+              { theme === "latte" ? (
               <label
                 htmlFor="switch-component"
-                className={`before:content[''] absolute top-2/4 -${position}-1 h-5 w-5 -translate-y-2/4 cursor-pointer rounded-full border border-blue-gray-100 bg-white shadow-md transition-all duration-300 before:absolute before:top-2/4 before:left-2/4 before:block before:h-10 before:w-10 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity hover:before:opacity-10 peer-checked:translate-x-full peer-checked:border-pink-500 peer-checked:before:bg-pink-500`}
+                className={`before:content[''] absolute top-2/4 -right-1 h-5 w-5 -translate-y-2/4 cursor-pointer rounded-full border border-blue-gray-100 bg-white shadow-md transition-all duration-300 before:absolute before:top-2/4 before:right-2/4 before:block before:h-10 before:w-10 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity hover:before:opacity-10 peer-checked:translate-x-0 peer-checked:border-pink-500 peer-checked:before:bg-pink-500`}
               >
                 <div
                   className="top-2/4 left-2/4 inline-block -translate-x-2/4 -translate-y-2/4 rounded-full p-5"
                   data-ripple-dark="true"
                 ></div>
               </label>
+              ) : (
+                <label
+                htmlFor="switch-component"
+                className={`before:content[''] absolute top-2/4 -left-1 h-5 w-5 -translate-y-2/4 cursor-pointer rounded-full border border-blue-gray-100 bg-white shadow-md transition-all duration-300 before:absolute before:top-2/4 before:left-2/4 before:block before:h-10 before:w-10 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity hover:before:opacity-10 peer-checked:translate-x-full peer-checked:border-pink-500 peer-checked:before:bg-pink-500`}
+              >
+                <div
+                  className="top-2/4 left-2/4 inline-block -translate-x-2/4 -translate-y-2/4 rounded-full p-5"
+                  data-ripple-dark="true"
+                ></div>
+              </label>
+              )}
             </div>
             {sun && sunLight ? (
             theme === 'mocha' ? (
