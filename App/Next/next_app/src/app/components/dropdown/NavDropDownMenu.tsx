8853c4b700792b01281ca6bc92e47548821cb773
Aurelien calandrino <aurelien.calandrino@outlook.fr>
2023-09-02 15:38:50 +0200

    profile logo light implemented

--- App/Next/next_app/src/app/components/dropdown/NavDropDownMenu.tsx
+++ App/Next/next_app/src/app/components/dropdown/NavDropDownMenu.tsx
@@ -2,6 +2,7 @@ import { useState, useRef, ReactNode } from 'react';
 import { clickOutsideHandler } from '@/app/hooks/clickOutsideHandler';
 import Image from "next/image"
 import ProfileIcon from "../../../../public/profile-svgrepo-com.svg"
+import ProfileIconLight from "../../../../public/profile-light.svg"
 
 const NavDropDownMenu = ({ children }: { children: ReactNode }) => {
     const wrapperRef = useRef<HTMLDivElement>(null);
@@ -15,7 +16,11 @@ const NavDropDownMenu = ({ children }: { children: ReactNode }) => {
                 type="button"
                 className="inline-flex justify-center w-full rounded-full px-2 py-2 bg-base hover:bg-crust hover:scale-105 transform transition"
                 onClick={() => setIsOpen(!isOpen)}>
-                    <Image src={ProfileIcon} width={32} height={32} alt="Profile" />
+                    { localStorage.getItem("theme") === "latte" ? (
+                        <Image src={ProfileIconLight} width={32} height={32} alt="Profile" />
+                    ) : (
+                        <Image src={ProfileIcon} width={32} height={32} alt="Profile" />
+                    )}
             </button>
             {isOpen && (
                 <div className="absolute z-10 mt-2 w-40 right-1 rounded-md bg-crust">
