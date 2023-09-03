18137126ea0e91ef0b3484a0f2f4cdd583283fc9
aurelien <aurelien.calandrino@outlook.fr>
2023-09-03 15:55:07 +0200

    check box is now persistent and fluent

--- App/Next/next_app/src/app/components/theme/Theme.tsx
+++ App/Next/next_app/src/app/components/theme/Theme.tsx
@@ -39,18 +39,8 @@ export const Theme = ({ children }: { children: React.ReactNode }) => {
                 type="checkbox"
                 className="peer absolute h-4 w-8 cursor-pointer appearance-none rounded-full bg-overlay0 transition-colors duration-300 checked:bg-pink-500 peer-checked:border-pink-500 peer-checked:before:bg-pink-500"
                 onClick={toggleTheme}
+                checked={theme === "mocha"}
               />
-              { theme === "latte" ? (
-              <label
-                htmlFor="switch-component"
-                className={`before:content[''] absolute top-2/4 -right-1 h-5 w-5 -translate-y-2/4 cursor-pointer rounded-full border border-blue-gray-100 bg-white shadow-md transition-all duration-300 before:absolute before:top-2/4 before:right-2/4 before:block before:h-10 before:w-10 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity hover:before:opacity-10 peer-checked:translate-x-0 peer-checked:border-pink-500 peer-checked:before:bg-pink-500`}
-              >
-                <div
-                  className="top-2/4 left-2/4 inline-block -translate-x-2/4 -translate-y-2/4 rounded-full p-5"
-                  data-ripple-dark="true"
-                ></div>
-              </label>
-              ) : (
                 <label
                 htmlFor="switch-component"
                 className={`before:content[''] absolute top-2/4 -left-1 h-5 w-5 -translate-y-2/4 cursor-pointer rounded-full border border-blue-gray-100 bg-white shadow-md transition-all duration-300 before:absolute before:top-2/4 before:left-2/4 before:block before:h-10 before:w-10 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity hover:before:opacity-10 peer-checked:translate-x-full peer-checked:border-pink-500 peer-checked:before:bg-pink-500`}
@@ -60,7 +50,6 @@ export const Theme = ({ children }: { children: React.ReactNode }) => {
                   data-ripple-dark="true"
                 ></div>
               </label>
-              )}
             </div>
             {sun && sunLight ? (
             theme === 'mocha' ? (
