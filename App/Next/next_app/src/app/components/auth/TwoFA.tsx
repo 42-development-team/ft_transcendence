9c57d0cfcdfaa402c34eeb744398afc7937d203a
aurelien <aurelien.calandrino@outlook.fr>
2023-09-05 13:05:01 +0200

    adjuste 2fa shadow

--- App/Next/next_app/src/app/components/auth/TwoFA.tsx
+++ App/Next/next_app/src/app/components/auth/TwoFA.tsx
@@ -161,7 +161,7 @@ const TwoFA = ({ userId }: { userId: string }) => {
 	}
 
 	return (
-		<div className="flex flex-col border-2 rounded-md bg-base shadow-[0_35px_90px_-10px_rgba(0,0,0,0.40)]">
+		<div className="flex flex-col border-0 rounded-md bg-base shadow-[0_35px_90px_-10px_rgba(0,0,0,0.25)]">
 			<div className="flex justify-center mt-2">
 				{
 					!activTwoFA &&
