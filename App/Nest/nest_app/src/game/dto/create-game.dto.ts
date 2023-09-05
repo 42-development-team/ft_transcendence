ff6492362391225a570934c11886b67f0f4eed76
aurelien <aurelien.calandrino@outlook.fr>
2023-09-05 11:29:36 +0200

    createDto is used in end of the a game

--- App/Nest/nest_app/src/game/dto/create-game.dto.ts
+++ App/Nest/nest_app/src/game/dto/create-game.dto.ts
@@ -1,5 +1,27 @@
+import { User } from "@prisma/client";
+import { IsNotEmpty, IsNumber } from "class-validator";
 
 export class CreateGameDto {
-    playerOneId: number;
-    playerTwoId: number;
+
+    @IsNotEmpty()
+    @IsNumber()
+    winnerScore: number;
+
+    @IsNotEmpty()
+    @IsNumber()
+    loserScore: number;
+
+    @IsNotEmpty()
+    winner: User;
+
+    @IsNotEmpty()
+    @IsNumber()    
+    winnerId: number;
+    
+    @IsNotEmpty()
+    @IsNumber()
+    loserId: number;
+
+    @IsNotEmpty()
+    loser: User;
 }
