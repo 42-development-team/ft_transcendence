3bc34b953d7d94048782a7f4e1b79d86d0f3cf07
aurelien <aurelien.calandrino@outlook.fr>
2023-09-05 13:01:58 +0200

    add a div in QrCode component because of warning

--- App/Next/next_app/src/app/components/auth/QrCodeDisplay.tsx
+++ App/Next/next_app/src/app/components/auth/QrCodeDisplay.tsx
@@ -2,17 +2,20 @@ import '../../globals.css'
 
 const QrCodeDisplay = ({children, imageUrl="", displayBox=false} : {children: any, imageUrl: string, displayBox: Boolean}) => {
     return (
-        imageUrl !== '' &&  displayBox && 
-        <div className=" flex justify-center">
-            <img className=" rounded shadow-[0_30px_60px_-10px_rgba(0,0,0,0.05)] border-2 border-surface0" 
-            placeholder={'/home/aurel/Documents/ft_transcendence/App/Next/next_app/public/logout-svgrepo-com.svg'} //TODO: add placeHolder accordingly
-            src={imageUrl} 
-            height="150" 
-            width="150" 
-            alt="QR Code" 
-            {...children}
-            />
-        </div> 
+        <div>
+            {imageUrl !== '' && displayBox &&
+                <div className=" flex justify-center">
+                    <img className=" rounded shadow-[0_30px_60px_-10px_rgba(0,0,0,0.05)] border-2 border-surface0"
+                        placeholder={'none'}
+                        src={imageUrl}
+                        height="150"
+                        width="150"
+                        alt="QR Code"
+                        {...children}
+                    />
+                </div>
+            }
+        </div>
     )
 }
 
