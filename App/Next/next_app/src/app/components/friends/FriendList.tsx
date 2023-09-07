80fd4e9a7914ab6130387b72d2a86baa295d401d
bl000m <mathiapagani@gmail.com>
2023-09-07 18:22:28 +0200

    retireving friends list from useFriend hook

--- App/Next/next_app/src/app/components/friends/FriendList.tsx
+++ App/Next/next_app/src/app/components/friends/FriendList.tsx
@@ -4,16 +4,18 @@ import FriendItem from "./FriendItem";
 import ChatHeader from "../chat/chatbox/ChatHeader";
 import ChatMemberHeader from "../chat/chatbox/members/ChatMemberHeader";
 import BlockUserItem from "./BlockUserItem";
+import useFriends from "@/app/hooks/useFriends";
 
 interface FriendListProps {
-    friends: UserModel[];
+    // friends: UserModel[];
     blockedUsers: UserModel[];
     unblockUser: (unblockedId: string) => void;
 }
 
-const FriendList = ({friends, blockedUsers, unblockUser}: FriendListProps) => {
+const FriendList = ({blockedUsers, unblockUser}: FriendListProps) => {
 
     const {updateChatBarState} = useChatBarContext();
+	const {friends} = useFriends();
 
     const friendsList = friends.map((friend) => (
         <FriendItem key={friend.id} user={friend}/>
@@ -22,7 +24,7 @@ const FriendList = ({friends, blockedUsers, unblockUser}: FriendListProps) => {
     const blockedList = blockedUsers.map((blockedUser) => (
         <BlockUserItem key={blockedUser.id} user={blockedUser} unblockUser={unblockUser}/>
     ))
-    
+
     return (
         <div className='w-[450px] h-full px-2 py-2 rounded-r-lg bg-base border-crust border-2'>
             <ChatHeader title="Friend List" onCollapse={() => updateChatBarState(ChatBarState.Closed)} />
