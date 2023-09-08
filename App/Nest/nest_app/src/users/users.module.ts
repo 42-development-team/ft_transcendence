f665419704ca0ee71ba228b64df52f7b43388f14
Martin Degraeuwe <mdegraeu@z1r9p5.42lyon.fr>
2023-09-08 11:56:49 +0200

    tests fixed

--- App/Nest/nest_app/src/users/users.module.ts
+++ App/Nest/nest_app/src/users/users.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [],
