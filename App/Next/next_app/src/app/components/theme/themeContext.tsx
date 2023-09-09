504c2960ed17b00e1b241c9e858a667755df1efb
Cpaluszek <paluszek.clement@gmail.com>
2023-09-09 17:46:44 +0200

    protect theme local storage

--- App/Next/next_app/src/app/components/theme/themeContext.tsx
+++ App/Next/next_app/src/app/components/theme/themeContext.tsx
@@ -2,8 +2,9 @@
     import { createContext } from 'react';
 
     const ThemeContext = createContext({
-      theme: (typeof window == 'undefined' && localStorage.getItem("theme")) || 'mocha',
+      theme: typeof window !== undefined ? "mocha" : localStorage.getItem("theme") || 'mocha',
       setTheme: (theme: any) => {}
     });
+
 
     export default ThemeContext;
