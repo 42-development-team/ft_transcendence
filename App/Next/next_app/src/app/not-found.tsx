a1a252941ff7b4d84e01703ba949db2c08a27102
Aurelien calandrino <aurelien.calandrino@outlook.fr>
2023-09-10 00:20:55 +0200

    add High 404

--- App/Next/next_app/src/app/not-found.tsx
+++ App/Next/next_app/src/app/not-found.tsx
@@ -1,9 +1,12 @@
 import Link from 'next/link';
+import notFoundHigh from '../../public/notFoundHigh.png';
+import Image from 'next/image';
 
 export default function NotFound() {
     return (
         <div className='flex flex-col gap-4'>
-            <h1 className='text-2xl'>404 - Page Not Found</h1>
+            <Image src={notFoundHigh} alt='404' width={500} height={500} className=' flex '/>
+            <h1 className='text-center text-2xl mb-4'>404 - Page Not Found</h1>
             <Link className='bg-mauve hover:bg-pink cursor-pointer drop-shadow-xl rounded text-base font-bold text-lg p-2 text-center
 		disabled:pointer-events-none disabled:opacity-50' href="/">
             Return home</Link>
