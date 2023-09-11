79ae1d7b7343c9a27feac3461e7b65eb5d9d7236
Aurelien calandrino <aurelien.calandrino@outlook.fr>
2023-09-11 17:50:16 +0200

    2 different set of background added, some other background remains for testing

--- App/Next/next_app/src/app/components/profile/tabs.tsx
+++ App/Next/next_app/src/app/components/profile/tabs.tsx
@@ -69,10 +69,10 @@ export function UnderlineTabs({ userId }: { userId: string }) {
   };
 
   return (
-    <div className=" backdrop-blur-lg mt-[1vw] rounded-lg transition hover:duration-[550ms] bg-surface0 bg-opacity-60 hover:shadow-[0_35px_55px_-20px_rgba(0,0,0,0.20)]">
+    <div className=" backdrop-blur-xl mt-[1vw] rounded-lg transition hover:duration-[550ms] bg-surface0 bg-opacity-60 hover:shadow-[0_35px_55px_-20px_rgba(0,0,0,0.20)]">
       <Tabs value={activeTab}>
         <TabsHeader
-          className="text-xl rounded-none bg-transparent p-0 font-semibold h-[4vh]"
+          className="text-xl rounded-none bg-surface0 p-0 font-semibold h-[4vh]"
         >
           {data.map(({ label, value }) => (
             <Tab
