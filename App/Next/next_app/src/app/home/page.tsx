21c1f3a61bb0b59167810b286262ac9fcb62ce1d
Aurelien calandrino <aurelien.calandrino@outlook.fr>
2023-09-19 14:06:41 +0200

    Pong is now a text, font import was not well imported, add woff format + trying to create a neon effect + play button come after

--- App/Next/next_app/src/app/home/page.tsx
+++ App/Next/next_app/src/app/home/page.tsx
@@ -1,18 +1,23 @@
 "use client"
 
 import Chat from "@/components/chat/Chat";
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useAuthContext } from "../context/AuthContext";
 import Play from "../components/home/play";
 import Game from "../components/game/Game";
 import useGame from "../hooks/useGame";
 import InGameContext from "../context/inGameContext";
-import Pong from "../../../public/pong.svg"
+import Pong from "../../../public/pong.png"
 import Image from "next/image";
 
 export default function Home() {
   const { login, userId } = useAuthContext();
   const { inGameContext } = useContext(InGameContext);
+  const [fontSize, setFontSize] = useState<number>(window.innerWidth / 9);
+
+  window.addEventListener('resize', () => {
+    setFontSize(window.innerWidth / 9);
+  });
 
   useEffect(() => {
     login();
@@ -23,9 +28,12 @@ export default function Home() {
   return (
     <div className="flex w-full h-full">
       <Chat userId={userId} />
-      { inGame === false && inGameContext === false ? (
-          <div className="w-full p-4 h-full flex items-center justify-center">
-            <img src={Pong.src} alt="Pong" className="w-1/2 h-1/2" />
+      {inGame === false && inGameContext === false ? (
+        <div className="w-full p-4 h-full flex flex-col items-center justify-evenly">
+          <div className="flex basis-1/6"/>
+            <div className='flex cyber text-yellow' style={{fontSize: fontSize + 'px', fontFamily: "Cy", textShadow:'4px 4px 8px'}}>PONG</div>
+          <div className="basis-1/5"/>
+          <div className="basis-3/6">
             <Play
               socket={socket}
               isUserQueued={isUserQueued}
@@ -36,22 +44,23 @@ export default function Home() {
               mode={mode}
             />
           </div>
-        ) : (
-          <div className="flex flex-col justify-center flex-grow h-full w-full">
-            <Game
-              socket={socket}
-              surrender={surrender}
-              move={move}
-              stopMove={stopMove}
-              launchGame={launchGame}
-              joinQueue={joinQueue}
-              data={data}
-              userId={userId}
-              result={result}
-              setResult={setResult}
-              setInGameContext={setInGameContext}
-            />
-          </div>
+        </div>
+      ) : (
+        <div className="flex flex-col justify-center flex-grow h-full w-full">
+          <Game
+            socket={socket}
+            surrender={surrender}
+            move={move}
+            stopMove={stopMove}
+            launchGame={launchGame}
+            joinQueue={joinQueue}
+            data={data}
+            userId={userId}
+            result={result}
+            setResult={setResult}
+            setInGameContext={setInGameContext}
+          />
+        </div>
       )}
     </div>
   );
