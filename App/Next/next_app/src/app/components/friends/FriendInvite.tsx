f9b1a9d2c853450a50b269da84c81c69f42ce1a3
Cpaluszek <paluszek.clement@gmail.com>
2023-09-20 11:34:48 +0200

    Cancel friend invite OK

--- App/Next/next_app/src/app/components/friends/FriendInvite.tsx
+++ App/Next/next_app/src/app/components/friends/FriendInvite.tsx
@@ -33,6 +33,14 @@ const FriendInvite = ({ user, hideActions }: FriendProps) => {
 		});
 	}
 
+	const cancelFriendRequest = async () => {
+		console.log("test");
+		await fetch(`${process.env.BACK_URL}/friend/cancelFriend/${user.id}`, {
+			credentials: "include",
+			method: "PATCH"
+		});
+	}
+
     return (
         <div className="flex flex-grow relative items-center justify-between mt-2 mb-2 hover:bg-surface1 rounded py-1 px-2 mr-2">
             <div className="flex items-center">
@@ -66,7 +74,7 @@ const FriendInvite = ({ user, hideActions }: FriendProps) => {
 			{hideActions &&
 			<div className="relative inline-block text-left">
 				<Tooltip placement="left" content="Cancel" className="tooltip">
-					<button onClick={() => handleAction(() => console.log("test"))} 
+					<button onClick={() => handleAction(cancelFriendRequest)}
 						className="inline-flex justify-center w-full rounded-2xl px-2 py-2 bg-base group" >
 						<svg aria-hidden="true" role="img" width="24" height="24" viewBox="0 0 24 24">
 							<path className="group-hover:fill-red" fill="currentColor" fillRule="evenodd" clipRule="evenodd" d="M20.2949 0.298996L21.7089 1.713L19.4169 4.006L21.7089 6.299L20.2949 7.713L18.0019 5.42L15.7099 7.713L14.2949 6.299L16.5879 4.006L14.2949 1.713L15.7099 0.298996L18.0019 2.592L20.2949 0.298996ZM8.00195 13.006C10.207 13.006 12.002 11.211 12.002 9.006C12.002 6.801 10.207 5.006 8.00195 5.006C5.79695 5.006 4.00195 6.801 4.00195 9.006C4.00195 11.211 5.79695 13.006 8.00195 13.006ZM8.00195 14.006C3.29095 14.006 0.00195312 16.473 0.00195312 20.006V21.006H16.002V20.006C16.002 16.473 12.713 14.006 8.00195 14.006Z"></path>
