cbd05828e56c8ccbae1c10c3359175823795ae43
Aurelien calandrino <aurelien.calandrino@outlook.fr>
2023-09-21 14:26:32 +0200

    fix color on refresh, fixing animation on receive

--- App/Next/next_app/src/app/components/game/sidePanelGameInvite.tsx
+++ App/Next/next_app/src/app/components/game/sidePanelGameInvite.tsx
@@ -11,11 +11,11 @@ const SidePanelGameInvite = () => {
     const { mode, invitedBy, respondToInvite, inviteSent, cancelInvite } = useContext(GameInviteContext);
     const [slide, setSlide] = useState("translateX(0%)"); //TODO : set to 100%
     const { theme } = useContext(ThemeContext);
-    const [backgroundColor, setBackgroundColor] = useState(theme === "latte" ? "black" : "white");
-    const [textColor, setTextColor] = useState(theme === "latte" ? "white" : "black");
-    const [borderColor, setBorderColor] = useState(theme === "latte" ? "white" : "black");
-    const [buttonColor, setButtonColor] = useState(theme === "latte" ? "bg-mauve" : "bg-pink");
-    const [hoverColor, setHoverColor] = useState(theme === "latte" ? "bg-pink" : "bg-mauve");
+    const [backgroundColor, setBackgroundColor] = useState(theme === "latte" ? "#6c6f85" : "#313244");
+    const [textColor, setTextColor] = useState(theme === "latte" ? "#eff1f5" : "#f9e2af");
+    const [borderColor, setBorderColor] = useState(theme === "latte" ? "#eff1f5" : "#f9e2af");
+    const [buttonColor, setButtonColor] = useState(theme === "latte" ? "bg-[#8839ef]" : "bg-[#f5c2e7]");
+    const [hoverColor, setHoverColor] = useState(theme === "latte" ? "hover:bg-[#ea76cb]" : "hover:bg-[#cba6f7]");
     const [disable, setDisable] = useState(false);
 
     const onChange = (accept: boolean) => {
@@ -52,43 +52,47 @@ const SidePanelGameInvite = () => {
 
     return (
         <div >
-            {/* {invitedBy !== "" && */}
-            <div
-                style={{
-                    position: 'fixed',
-                    borderLeft: `1px solid ${borderColor}`,
-                    borderTop: `1px solid ${borderColor}`,
-                    borderBottom: `1px solid ${borderColor}`,
-                    borderTopLeftRadius: '10px',
-                    borderBottomLeftRadius: '10px',
-                    borderRight: 'none',
-                    top: 70,
-                    right: 0,
-                    width: '301px',
-                    height: '85px',
-                    backgroundColor: backgroundColor,
-                    padding: '2px',
-                    boxSizing: 'border-box',
-                    transform: slide,
-                    transition: 'transform 0.7s ease-out',
-                    color: textColor,
-                    opacity: 0.9,
-                }}>
-                < div className="flex flex-col">
-                    <div className="flex  justify-center my-1">
-                        {invitedBy} want to play
-                    </div>
-                    <div className="flex justify-evenly w-full flex-row">
-                        <CustomBtnGameInvite text="ACCEPT" response={true} disable={disable} onChange={onChange} buttonColor={buttonColor} hoverColor={hoverColor} />
-                        <CustomBtnGameInvite text="DECLINE" response={false} disable={disable} onChange={onChange} buttonColor={buttonColor} hoverColor={hoverColor} />
-                    </div>
+            {(inviteSent || invitedBy) &&
+                <div
+                    style={{
+                        position: 'fixed',
+                        borderLeft: `1px solid ${borderColor}`,
+                        borderTop: `1px solid ${borderColor}`,
+                        borderBottom: `1px solid ${borderColor}`,
+                        borderTopLeftRadius: '10px',
+                        borderBottomLeftRadius: '10px',
+                        borderRight: 'none',
+                        top: 70,
+                        right: 0,
+                        width: '301px',
+                        height: '85px',
+                        backgroundColor: backgroundColor,
+                        padding: '2px',
+                        boxSizing: 'border-box',
+                        transform: slide,
+                        transition: 'transform 0.7s ease-out',
+                        color: textColor,
+                        opacity: 0.9,
+                    }}>
+                    {!inviteSent &&
+                        < div className="flex flex-col">
+                            <div className="flex  justify-center my-1">
+                                {invitedBy} want to play
+                            </div>
+                            <div className="flex justify-evenly w-full flex-row">
+                                <CustomBtnGameInvite text="ACCEPT" response={true} disable={disable} onChange={onChange} buttonColor={buttonColor} hoverColor={hoverColor} />
+                                <CustomBtnGameInvite text="DECLINE" response={false} disable={disable} onChange={onChange} buttonColor={buttonColor} hoverColor={hoverColor} />
+                            </div>
+                        </div>
+                    }
+                    {inviteSent &&
+                        <div className="flex flex-col items-center justify-center my-1">
+                            Waiting for user... {/* here put username */}
+                        <CustomBtnGameInvite text="CANCEL" response={false} disable={false} onChange={cancel} buttonColor={buttonColor} hoverColor={hoverColor} />
+                        </div>
+                    }
                 </div>
-
-                {/* } */}
-                {inviteSent &&
-                    <CustomBtnGameInvite text="CANCEL" response={false} disable={false} onChange={cancel} buttonColor={buttonColor} hoverColor={hoverColor} />
-                }
-            </div>
+            }
         </div>
     )
 }
