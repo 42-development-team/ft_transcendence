a5571590c6216005fabb4921366984f4acf96507
Aurelien calandrino <aurelien.calandrino@outlook.fr>
2023-09-21 12:20:10 +0200

    add color change, todo: find good color + add border +  rounded ?

--- App/Next/next_app/src/app/components/game/sidePanelGameInvite.tsx
+++ App/Next/next_app/src/app/components/game/sidePanelGameInvite.tsx
@@ -2,12 +2,17 @@
 
 import { useAuthContext } from "@/app/context/AuthContext";
 import GameInviteContext from "@/app/context/GameInviteContext";
-import { useContext, useState } from "react"
+import { use, useContext, useEffect, useState } from "react"
+import ThemeContext from "../theme/themeContext";
+import { text } from "stream/consumers";
 
 const SidePanelGameInvite = () => {
     const { userId } = useAuthContext();
     const { mode, invitedBy, respondToInvite, inviteSent, cancelInvite } = useContext(GameInviteContext);
-    const [slide, setSlide] = useState("translateX(0%)");
+    const [slide, setSlide] = useState("translateX(0%)"); //TODO : set to 100%
+    const {theme} = useContext(ThemeContext);
+    const [backgroundColor, setBackgroundColor] = useState(theme === "latte" ? "black" : "white");
+    const [textColor, setTextColor] = useState(theme === "latte" ? "white" : "black");
 
     const onChange = (accept: boolean) => {
         setSlide("translateX(100%)");
@@ -23,29 +28,62 @@ const SidePanelGameInvite = () => {
         cancelInvite(userId)
     }
 
+    useEffect(() => {
+        if (theme === "latte") {
+            setBackgroundColor("black");
+            setTextColor("white");
+        } else {
+            setBackgroundColor("white");
+            setTextColor("black");
+        }
+    }, [theme])
+    
     return (
-        <div>
+        <div >
             {/* {invitedBy !== "" && */}
-                <div style={{position: 'fixed', borderColor: 'blue', border: '4px', flex: 'col',  top: 120, right: 0, width: '301px', height: '80px', backgroundColor: 'orange', padding: '20px', boxSizing: 'border-box', transform: slide, transition: 'transform 0.3s ease-out'}}>
-                    <div className="m-2">
+            <div
+            style={{
+                position: 'fixed',
+                borderColor: 'blue',
+                border: '4px',
+                top: 70,
+                right: 0,
+                width: '301px',
+                height: '80px',
+                backgroundColor: backgroundColor,
+                padding: '2px',
+                boxSizing: 'border-box',
+                transform: slide,
+                transition: 'transform 0.3s ease-out',
+                color: textColor,
+            }}>
+                < div className="flex flex-col">
+                    <div className="flex mb-2">
+                        Invited by {invitedBy}
+                    </div>
+                    <div className="flex flex-row">
+                <div className="m-2">
                     <button onClick={() => onChange(true)} >
                         ACCEPT
                     </button>
-                    </div>
-                    <div className="m-2">
-                        <button onClick={() => onChange(false)}>
-                            DECLINE
-                        </button>
+                </div>
+                <div className="m-2">
+                    <button onClick={() => onChange(false)}>
+                        DECLINE
+                    </button>
                     </div>
                 </div>
+                </div>
+            
             {/* } */}
             {inviteSent &&
                 <div className="m-2">
-                    <button onClick={ cancel }>
+                    <button onClick={cancel}>
                         CANCEL
                     </button>
                 </div>
             }
+            </div>
         </div>
     )
 }
