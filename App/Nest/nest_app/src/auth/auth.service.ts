7ada34d2c4fb4daeaa98eeaff858ac69b995dc14
Cpaluszek <paluszek.clement@gmail.com>
2023-09-21 14:08:21 +0200

    remove rt cookie

--- App/Nest/nest_app/src/auth/auth.service.ts
+++ App/Nest/nest_app/src/auth/auth.service.ts
@@ -40,12 +40,11 @@ export class AuthService {
                     .redirect(`${frontUrl}/auth/2fa`);
             }
             else {
-                // Todo: remove rt
                 jwt = await this.getTokens(userDB, true);
                 res.status(200)
                     .cookie("jwt", jwt.access_token, cookieOptions)
-                    .cookie("rt", jwt.refresh_token, cookieOptions)
                     .redirect(`${frontUrl}/home/`);
+                    // .cookie("rt", jwt.refresh_token, cookieOptions)
             }
         } catch (error) {
             throw new Error('Redirect error');
