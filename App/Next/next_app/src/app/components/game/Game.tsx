a139eb3a27031caf1a9142406f755c265a7a8e4c
Aurelien calandrino <aurelien.calandrino@outlook.fr>
2023-09-24 15:58:42 +0200

    opponentUsername well found

--- App/Next/next_app/src/app/components/game/Game.tsx
+++ App/Next/next_app/src/app/components/game/Game.tsx
@@ -19,6 +19,14 @@ const Game = ({ ...props }) => {
 		getUserNameById(props.userId).then((userName: SetStateAction<string>) => {
 			setUserName(userName);
 		});
+		if ( data.player1.id === parseInt(props.userId))
+			getUserNameById(data.player2.id).then((userName: SetStateAction<string>) => {
+				setOpponnentUsername(userName);
+			});
+		else
+			getUserNameById(data.player1.id).then((userName: SetStateAction<string>) => {
+				setOpponnentUsername(userName);
+			});
 	}, []);
 
 	const { socket, surrender, move, stopMove, launchGame, joinQueue, data, mode, userId, result, setResult, setInGameContext } = props;
@@ -34,7 +42,7 @@ const Game = ({ ...props }) => {
 							data={data}
 						/>
 						<div className="flex flex-row justify-evenly mb-2 mx-[18vw]">
-							<div className="flex ">{opponnentUsername}</div>
+							<div className="flex text-[2.2vw] text-mauve">{opponnentUsername}</div>
 							<div className="flex text-[2.2vw] text-mauve">{userName}</div>
 						</div>
 						<Canvas
