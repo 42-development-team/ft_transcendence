c42fa447411db9cccd62d0696e26be1420ed0a50
Aurelien calandrino <aurelien.calandrino@outlook.fr>
2023-09-24 15:08:47 +0200

    logo on result well positionned + flex-grow on game => game is smaller on full screen, seems better + Logo component cleaned and remove useless import

--- App/Next/next_app/src/app/components/home/Logo.tsx
+++ App/Next/next_app/src/app/components/home/Logo.tsx
@@ -1,5 +1,27 @@
+"use client";
 
-const Logo = ( { colorText, neonColor }: { colorText: string, neonColor: string}) => {
+import { useContext, useEffect, useState } from "react";
+import themeContext from "../theme/themeContext";
+
+
+
+const Logo = () => {
+
+    const { theme } = useContext(themeContext);
+    let storage = typeof window !== "undefined" ? localStorage.getItem("theme") : "mocha";
+    const [colorText, setColorText] = useState<string>(storage === "latte" ? "text-[#e7a446]" : "text-[#f0f471]");
+    const [neonColor, setNeonColor] = useState<string>(storage === "latte" ? "#e7a446" : "#0073e6");
+
+    useEffect(() => {
+        if (theme === "latte") {
+            setColorText("text-[#e7a446]");
+            setNeonColor("#ea76cb");
+        } else {
+            setColorText("text-[#e7a446]");
+            setNeonColor("#cba6f9");
+        }
+    }, [theme]);
+
     return (
         <div
             className={`flex cyber pointer-events-none ` + colorText}
--- App/Next/next_app/src/app/home/page.tsx
+++ App/Next/next_app/src/app/home/page.tsx
@@ -7,7 +7,6 @@ import Play from "../components/home/play";
 import Game from "../components/game/Game";
 import useGame from "../hooks/useGame";
 import InGameContext from "../context/inGameContext";
-import themeContext from "../components/theme/themeContext";
 import useFriends from "@/hooks/useFriends";
 import Logo from "../components/home/Logo";
 
@@ -15,36 +14,10 @@ export default function Home() {
 	const { login, userId } = useAuthContext();
 	const { inGameContext } = useContext(InGameContext);
 	const { friends, invitedFriends, requestedFriends, addFriend, blockedUsers, blockUser, unblockUser } = useFriends();
-	const { theme } = useContext(themeContext);
-	let storage = typeof window !== "undefined" ? localStorage.getItem("theme") : "mocha";
-	const [colorText, setColorText] = useState<string>(storage === "latte" ? "text-[#e7a446]" : "text-[#f0f471]");
-	const [neonColor, setNeonColor] = useState<string>(storage === "latte" ? "#e7a446" : "#0073e6");
-	// const [loading, setLoading] = useState<boolean>(true);
-	// const [pSpace, setPSpace] = useState<number>(-15);
-
-	useEffect(() => {
-		if (theme === "latte") {
-			setColorText("text-[#e7a446]");
-			setNeonColor("#ea76cb");
-		} else {
-			setColorText("text-[#e7a446]");
-			setNeonColor("#cba6f9");
-		}
-	}, [theme]);
-
-	// const handleResize = async () => {
-	// 	if (window.innerWidth < 1000)
-	// 		setPSpace(-15);
-	// 	else {
-	// 		setPSpace(-35);
-	// 	}
-	// }
 
 	useEffect(() => {
 		if (typeof window === 'undefined') return;
-		// handleResize();
 		login();
-		// handleResize();
 	}, []);
 
 	const { surrender, move, stopMove, leaveQueue, joinQueue, isUserQueued, launchGame, socket, inGame, setInGameContext, result, setResult, data, changeMode, mode } = useGame();
@@ -56,7 +29,7 @@ export default function Home() {
 			{inGame === false && inGameContext === false ? (
 				<div className="w-full p-4 h-full flex flex-col items-center justify-evenly">
 					<div className="flex basis-1/6" />
-					<Logo colorText={colorText} neonColor={neonColor} />	
+					<Logo />	
 					<div className="basis-1/5" />
 					<div className="basis-3/6">
 						<Play
@@ -71,7 +44,7 @@ export default function Home() {
 					</div>
 				</div>
 			) : (
-				<div className="flex flex-col justify-center flex-grow h-full w-full">
+				<div className="flex justify-center fex-grow h-full w-full">
 					<Game
 						socket={socket}
 						surrender={surrender}
