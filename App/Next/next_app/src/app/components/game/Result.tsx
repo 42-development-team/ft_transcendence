a5c8d494d7af4d25687503e892e2a44618894da6
Aurelien calandrino <aurelien.calandrino@outlook.fr>
2023-09-25 12:54:03 +0200

    displayresultData contain all the scores display result, avatar..

--- App/Next/next_app/src/app/components/game/Result.tsx
+++ App/Next/next_app/src/app/components/game/Result.tsx
@@ -6,6 +6,7 @@ import getAvatarById from "../utils/getAvatarById";
 import { useEffect, useState } from "react";
 import CustomBtn from "../CustomBtn";
 import Logo from "../home/Logo";
+import { DisplayResultData } from "./DisplayResultData";
 
 
 const Result = ({ ...props }) => {
@@ -48,37 +49,7 @@ const Result = ({ ...props }) => {
 
             </div>
             <div className="flex flex-col ">
-                <div className="flex flex-row justify-between bg-base rounded-lg bg-opacity-80 backdrop-blur-sm">
-                    <div className="flex">
-                        {user ? (
-                            <div className="flex flex-col text-center ml-[4vw] ">
-                                <Avatar
-                                    width={64} height={64} imageUrlGetFromCloudinary={user.avatar} disableChooseAvatar={true} disableImageResize={true} currId={user.id} isOnProfilePage={false}
-                                />
-                                {user.userName}
-                            </div>
-                        ) : (
-                            <div className="flex w-16 h-16 bg-crust rounded-full animate-pulse">Loading..</div>
-                        )
-                        }
-                    </div>
-                    <div className="flex flex-row items-center">
-                        {user ? user.score : "Loading..."} - {opponent ? opponent.score : "Loading.."}
-                    </div>
-                    <div className="flex flex-row">
-                        {opponent ? (
-                            <div className="flex flex-col text-center mr-[4vw]">
-                                <Avatar
-                                    width={64} height={64} imageUrlGetFromCloudinary={opponent.avatar} disableChooseAvatar={true} disableImageResize={true} currId={opponent.id} isOnProfilePage={false}
-                                />
-                                {opponent.userName}
-                            </div>
-                        ) : (
-                            <div className="flex w-16 h-16 bg-crust rounded-full animate-pulse">Loading..</div>
-                        )
-                        }
-                    </div>
-                </div>
+                <DisplayResultData  user={user} opponent={opponent} />
                 <CustomBtn
                     anim={true}
                     color={'bg-mauve'}
