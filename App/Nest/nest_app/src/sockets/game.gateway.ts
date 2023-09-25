45ace5e3f89a715e5b0df31d15e5b26aaed0ab09
Aurelien calandrino <aurelien.calandrino@outlook.fr>
2023-09-25 18:01:46 +0200

    warning disable, add reactStrict mode =  false in next config, hide warningm, if we want to solve problem at root, we have to find out wich lib is doing that

--- App/Nest/nest_app/src/sockets/game.gateway.ts
+++ App/Nest/nest_app/src/sockets/game.gateway.ts
@@ -267,7 +267,7 @@ export class GameGateway implements OnGatewayConnection, OnGatewayDisconnect {
     // ========= GAME LOGIC =========//
 
     async sleepAndCalculate(data: GameDto): Promise<GameDto> {
-        const promiseSleep = this.gameService.sleep(1000 / 60);
+        const promiseSleep = this.gameService.sleep(1000/60);
         const promiseCalculate = this.gameService.calculateGame(data.id, data.mode);
 
         await Promise.all([promiseSleep, promiseCalculate]);
