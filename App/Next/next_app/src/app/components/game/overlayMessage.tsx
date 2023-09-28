cfe6255a3a37ffca1340fa1e1f3e14e5222bcc6f
Aurelien calandrino <aurelien.calandrino@outlook.fr>
2023-09-28 23:18:59 +0200

    fixed surrender button receive string instead of int

--- App/Next/next_app/src/app/components/game/overlayMessage.tsx
+++ App/Next/next_app/src/app/components/game/overlayMessage.tsx
@@ -3,7 +3,7 @@ import React, { use, useContext, useEffect, useState } from 'react';
 import ThemeContext from '../theme/themeContext';
 import CustomBtn from '../CustomBtn';
 
-const OverlayMessage = ({ message, surrender, id, userId } : { message: any, surrender: (id: number, userId: number) => void, id: number, userId: number}) => {
+const OverlayMessage = ({ message, surrender, id, userId } : { message: any, surrender: (id: number, userId: number) => void, id: string, userId: string}) => {
     const {theme} = useContext(ThemeContext);
     const [textColor, setTextColor] = useState<string>(theme === 'latte' ? 'text-gray-100' : 'text-text')
     useEffect(() => {
@@ -20,7 +20,7 @@ const OverlayMessage = ({ message, surrender, id, userId } : { message: any, sur
     <div className={`fixed inset-0 flex flex-col items-center justify-center w-[auto] h-[auto] z-1 ${textColor} bg-base bg-opacity-20 backdrop-blur-xl`}>
       <div className="flex text-xl sm:text-2xl lg:text-3xl xl:text-4xl text-center">{message}</div>
         <div className='flex'>
-            <CustomBtn onClick={() => surrender(0, 0)} color='bg-red' disable={false} >Surrender</CustomBtn>
+            <CustomBtn onClick={() => surrender(parseInt(id), parseInt(userId))} color='bg-red' disable={false} >Surrender</CustomBtn>
         </div>
     </div>
   );
